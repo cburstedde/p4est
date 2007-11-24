c4697a5a67bdb2ddc9d5bccaa9ac2ea5e55c6b39
Lucas C Wilcox <lucasw@ices.utexas.edu>
2007-11-23 19:20:57 -0600

    Add the connectivity allocation routines.

--- src/p4est.c
+++ src/p4est.c
@@ -1,4 +1,34 @@
 
 #include <p4est.h>
+#include <p4est_base.h>
+
+p4est_connectivity_t *
+p4est_connectivity_new (int32_t num_trees, int32_t num_vertices)
+{
+  p4est_connectivity_t *connectivity;
+
+  connectivity = P4EST_ALLOC (p4est_connectivity_t, 1);
+
+  connectivity->num_trees = num_trees;
+  connectivity->num_vertices = num_vertices;
+
+  connectivity->tree_to_vertex = P4EST_ALLOC (int32_t, 4 * num_trees);
+  connectivity->tree_to_tree = P4EST_ALLOC (int32_t, 4 * num_trees);
+  connectivity->tree_to_face = P4EST_ALLOC (int8_t, 4 * num_trees);
+
+  return connectivity;
+}
+
+void
+p4est_connectivity_destroy (p4est_connectivity_t * connectivity)
+{
+  P4EST_FREE (connectivity->tree_to_face);
+  P4EST_FREE (connectivity->tree_to_tree);
+  P4EST_FREE (connectivity->tree_to_vertex);
+
+  P4EST_FREE (connectivity);
+
+  return;
+}
 
 /* EOF p4est.c */
