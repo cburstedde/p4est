d42a5e2207f8fe34e6606c0a1df11ee871d9a0f2
carsten <carsten@ices.utexas.edu>
2007-11-23 18:29:20 -0600

    Removed the memory handler prototypes.

--- src/p4est_memory.h
+++ src/p4est_memory.h
@@ -7,16 +7,6 @@
 #include <p4est_obstack.h>
 
 /*
- * Prototypes for memory allocation functions.
- * p4est_array and p4est_mempool print an error msg to stderr and call abort()
- *    if memory allocation functions fail. This can be prevented by providing
- *    functions that never return (i.e. call longjmp() or abort() internally).
- */
-typedef void       *(*p4est_malloc_t) (size_t size);
-typedef void       *(*p4est_realloc_t) (void *ptr, size_t size);
-typedef void       *(*p4est_free_t) (void *ptr);
-
-/*
  * The p4est_array object provides a large array of equal-size elements.
  * The array can be resized.
  * Elements are accessed by their 0-based index, their address may change.
@@ -31,17 +21,10 @@ typedef struct p4est_array
   /* implementation variables */
   int                 elem_alloc;       /* number of allocated elements */
   void               *array;    /* linear array to store elements */
-
-  /* allocation functions */
-  p4est_malloc_t      my_malloc;
-  p4est_realloc_t     my_realloc;
-  p4est_free_t        my_free;
 }
 p4est_array_t;
 
-p4est_array_t      *p4est_array_new (int elem_size, p4est_malloc_t * malloc,
-                                     p4est_realloc_t * realloc,
-                                     p4est_free_t * free);
+p4est_array_t      *p4est_array_new (int elem_size);
 void                p4est_array_destroy (p4est_array_t * array);
 
 void                p4est_array_grow1 (p4est_array_t * array);
@@ -68,18 +51,11 @@ typedef struct p4est_mempool
   /* implementation variables */
   struct obstack      stack;    /* holds the allocated elements */
   p4est_array_t       freed;    /* buffers the freed elements */
-
-  /* allocation functions */
-  p4est_malloc_t      my_malloc;
-  p4est_realloc_t     my_realloc;
-  p4est_free_t        my_free;
 }
 p4est_mempool_t;
 
 /* create, destroy or reset to count=0 the memory pool */
-p4est_mempool_t    *p4est_mempool_new (int elem_size, p4est_malloc_t * malloc,
-                                       p4est_realloc_t * realloc,
-                                       p4est_free_t * free);
+p4est_mempool_t    *p4est_mempool_new (int elem_size);
 void                p4est_mempool_destroy (p4est_mempool_t * mempool);
 void                p4est_mempool_reset (p4est_mempool_t * mempool);
 
