4372ce3cad2cd49811301278a130651d314557f9
Lucas C Wilcox <lucasw@ices.utexas.edu>
2007-11-23 18:40:55 -0600

    Define p4est malloc macros.

--- src/p4est_base.h
+++ src/p4est_base.h
@@ -6,4 +6,13 @@
 #include <p4est_config.h>
 #endif
 
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#define P4EST_ALLOC(t,n) (t *) malloc ((n) * sizeof(t))
+#define P4EST_ALLOC_ZERO(t,n) (t *) calloc ((n), sizeof(t))
+#define P4EST_REALLOC(p,t,n) (t *) realloc ((p), (n) * sizeof(t))
+#define P4EST_FREE(p) free (p)
+
 #endif /* !__P4EST_BASE_H__ */
