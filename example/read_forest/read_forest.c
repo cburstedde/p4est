0c8900893985d0e224a3f1c0fc0a5c0b7febf1f4
carsten <carsten@ices.utexas.edu>
2007-12-03 12:41:37 -0600

    Added GPL v3 license and copyright information.

--- example/read_forest/read_forest.c
+++ example/read_forest/read_forest.c
@@ -1,3 +1,22 @@
+/*
+  This file is part of p4est.
+  p4est is a C library to manage a parallel collection of quadtrees and/or octrees.
+
+  Copyright (C) 2007 Carsten Burstedde, Lucas Wilcox.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <p4est.h>
 #include <p4est_file.h>
