ac707f8b4a8cfa20af9d8c34d49f6a22da0a2a06
Lucas C Wilcox <lucasw@ices.utexas.edu>
2007-12-03 17:34:24 -0600

    Have read_forest read the mesh from a temporary file.
    This allows for running the program without a mesh file.
    The only requirement is that the cwd is writable.

--- example/read_forest/read_forest.c
+++ example/read_forest/read_forest.c
@@ -19,22 +19,147 @@
 */
 
 #include <p4est.h>
-#include <p4est_file.h>
+#include <p4est_algorithms.h>
 #include <p4est_base.h>
+#include <p4est_file.h>
+
+#include <unistd.h>
+
+typedef struct
+{
+  int32_t             a;
+}
+user_data_t;
+
+static void
+init_fn (p4est_t * p4est, int32_t which_tree, p4est_quadrant_t * quadrant)
+{
+  user_data_t        *data = quadrant->user_data;
+
+  data->a = which_tree;
+}
 
 int
-main (void)
+main (int argc, char **argv)
 {
   int                 retval;
+  int                 rank = 0;
 
+#ifdef HAVE_MPI
+  int                 use_mpi = 1;
+  int                 mpiret;
+#endif
+  MPI_Comm            mpicomm;
+  p4est_t            *p4est;
   p4est_connectivity_t *connectivity;
 
-  retval = p4est_connectivity_read ("mesh.p4t", &connectivity);
+  mpicomm = MPI_COMM_NULL;
+#ifdef HAVE_MPI
+  if (use_mpi) {
+    mpiret = MPI_Init (&argc, &argv);
+    P4EST_CHECK_MPI (mpiret);
+    mpicomm = MPI_COMM_WORLD;
+  }
+#endif
+
+  char                template[] = "p4est_meshXXXXXX";
+  char                mesh[] = "		[Forest Info] # ]] [[ ]]\n"
+    "ver = 0.0.1  # Version of the forest file\n"
+    "Nk  = 3      # Number of elements\n"
+    "Nv  = 7      # Number of mesh vertices\n"
+    "Net = 0      # Number of element tags\n"
+    "Nft = 0      # Number of face tags\n"
+    "Ncf = 0      # Number of curved faces\n"
+    "Nct = 0      # Number of curved types\n"
+    "\n"
+    "                          [Coordinates of Element Vertices]\n"
+    "1 -1.00000000000e+00 -1.00000000000e+00\n"
+    "2  0.00000000000e+00 -1.00000000000e+00\n"
+    "3  0.00000000000e+00  0.00000000000e+00\n"
+    "4  1.00000000000e+00  0.00000000000e+00\n"
+    "5  1.00000000000e+00  1.00000000000e+00\n"
+    "6  0.00000000000e+00  1.00000000000e+00\n"
+    "7 -1.00000000000e+00  0.00000000000e+00\n"
+    "   [Element to Vertex]\n"
+    "1     1   2   4   3\n"
+    "2     1   3   6   7\n"
+    "3     3   4   5   6\n"
+    "  [Element to Element]\n"
+    "1     1   1   3   2\n"
+    "2     1   3   2   2\n"
+    "3     1   3   3   2\n"
+    "\n"
+    "[Element to Face]\n"
+    "1     1   2   1   1\n"
+    "2     4   4   3   4\n"
+    "3     3   2   3   2\n"
+    "\n"
+    "[Element Tags]\n" "[Face Tags]\n" "[Curved Faces]\n" "[Curved Types]\n";
+
+  int                 fd;
+  size_t              meshlength;
+  size_t              templatelength;
+
+#ifdef HAVE_MPI
+  mpiret = MPI_Comm_rank (mpicomm, &rank);
+  P4EST_CHECK_MPI (mpiret);
+#endif
+
+  if (rank == 0) {
+    /* Make a temporary file to hold the mesh */
+    fd = mkstemp (template);
+    P4EST_CHECK_ABORT (fd != -1, "Unable to open temp mesh file.");
+
+    /* Write out to the mesh to the temporary file */
+    meshlength = strlen (mesh);
+    retval = write (fd, mesh, meshlength);
+    P4EST_CHECK_ABORT (retval != -1, "Unable to write to temp mesh file.");
+
+    /* Close the temporary file */
+    retval = close (fd);
+    P4EST_CHECK_ABORT (!retval, "Unable to close the temp mesh file.");
+  }
+
+#ifdef HAVE_MPI
+  templatelength = strlen (template);
+  mpiret = MPI_Bcast (template, templatelength + 1, MPI_CHAR, 0, mpicomm);
+  P4EST_CHECK_MPI (mpiret);
+#endif
+
+  /* Read in the mesh into connectivity information */
+  retval = p4est_connectivity_read (template, &connectivity);
   P4EST_CHECK_ABORT (!retval, "Unable to read the mesh file.");
 
-  p4est_connectivity_print (connectivity);
+  /* Print the connectivity */
 
+  if (rank == 0) {
+    p4est_connectivity_print (connectivity);
+  }
+
+  p4est = p4est_new (mpicomm, stdout, connectivity,
+                     sizeof (user_data_t), init_fn);
+  p4est_tree_print (p4est_array_index (p4est->trees, 0),
+                    p4est->mpirank, stdout);
+
+  /* destroy the p4est and its connectivity structure */
+  p4est_destroy (p4est);
   p4est_connectivity_destroy (connectivity);
+
+  if (rank == 0) {
+    /* unlink the temporary file */
+    retval = unlink (template);
+    P4EST_CHECK_ABORT (!retval, "Unable to close the temp mesh file.");
+  }
+
+  /* clean up and exit */
+  p4est_memory_check ();
+
+#ifdef HAVE_MPI
+  if (use_mpi) {
+    mpiret = MPI_Finalize ();
+    P4EST_CHECK_MPI (mpiret);
+  }
+#endif
 
   return 0;
 }
