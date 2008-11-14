a7350670745e0414b0ced690d6d4deb44ae6a663
Carsten Burstedde <carsten@ices.utexas.edu>
2008-11-14 15:37:32 -0600

    Make p4est_init print the build configuration.

--- src/p4est_base.c
+++ src/p4est_base.c
@@ -214,11 +214,30 @@ p4est_init_logging (FILE * stream, int identifier)
 void
 p4est_init (sc_log_handler_t log_handler, int log_threshold)
 {
+  int                 w;
+  const char        **on, **ov;
+  const char         *overrides[] = { P4EST_OVERRIDES NULL, NULL };
+
   p4est_package_id = sc_package_register (log_handler, log_threshold,
                                           "p4est", "A forest of octrees");
-#ifdef P4EST_DEBUG
-  P4EST_GLOBAL_PRODUCTION ("P4EST_DEBUG is ON\n");
-#endif
+
+  w = 24;
+  P4EST_GLOBAL_PRODUCTIONF ("%s\n", P4EST_PACKAGE_STRING);
+  P4EST_GLOBAL_PRODUCTIONF ("%-*s %s\n", w, "CC", P4EST_CC);
+  P4EST_GLOBAL_PRODUCTIONF ("%-*s %s\n", w, "C_VERSION", P4EST_C_VERSION);
+  P4EST_GLOBAL_PRODUCTIONF ("%-*s %s\n", w, "CFLAGS", P4EST_CFLAGS);
+  P4EST_GLOBAL_PRODUCTIONF ("%-*s %s\n", w, "CPP", P4EST_CPP);
+  P4EST_GLOBAL_PRODUCTIONF ("%-*s %s\n", w, "CPPFLAGS", P4EST_CPPFLAGS);
+  P4EST_GLOBAL_PRODUCTIONF ("%-*s %s\n", w, "LINK", P4EST_LINK);
+  P4EST_GLOBAL_PRODUCTIONF ("%-*s %s\n", w, "LDFLAGS", P4EST_LDFLAGS);
+  P4EST_GLOBAL_PRODUCTIONF ("%-*s %s\n", w, "LIBS", P4EST_LIBS);
+
+  w = 32;
+  for (on = overrides; *on != NULL; on = ov + 1) {
+    ov = on + 1;
+    SC_CHECK_ABORT (*ov != NULL, "P4EST_OVERRIDES should contain pairs");
+    P4EST_GLOBAL_PRODUCTIONF ("%-*s %s\n", w, *on, *ov);
+  }
 }
 
 /* EOF p4est_base.c */
