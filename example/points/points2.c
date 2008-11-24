df1bd612d46c91aaaed7553ef1cb2d447ff1f754
Carsten Burstedde <carsten@ices.utexas.edu>
2008-11-23 23:39:25 -0600

    Started example for point based octree construction.

---
+++ example/points/points2.c
@@ -0,0 +1,136 @@
+/*
+  This file is part of p4est.
+  p4est is a C library to manage a parallel collection of quadtrees and/or
+  octrees.
+
+  Copyright (C) 2008 Carsten Burstedde, Lucas Wilcox.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <p4est_bits.h>
+#include <p4est_points.h>
+#include <p4est_vtk.h>
+#include <sc_io.h>
+
+static p4est_quadrant_t *
+read_points (const char *filename, p4est_locidx_t * num_points)
+{
+  int                 retval;
+  int                 qshift;
+  unsigned            ucount, u, ignored;
+  double              x, y, z;
+  double              qlen;
+  double             *point_buffer;
+  p4est_quadrant_t   *points, *q;
+  FILE               *file;
+
+  file = fopen (filename, "rb");
+  SC_CHECK_ABORTF (file != NULL, "Open file %s", filename);
+
+  sc_fread (&ucount, sizeof (unsigned int), 1, file, "Read point count");
+
+  point_buffer = P4EST_ALLOC (double, 3 * ucount);
+  sc_fread (point_buffer, sizeof (double), (size_t) (3 * ucount), file,
+            "Read points");
+
+  retval = fclose (file);
+  SC_CHECK_ABORTF (retval == 0, "Close file %s", filename);
+
+  q = points = P4EST_ALLOC_ZERO (p4est_quadrant_t, ucount);
+  qlen = (double) (1 << P4EST_QMAXLEVEL);
+  qshift = P4EST_MAXLEVEL - P4EST_QMAXLEVEL;
+  for (ignored = u = 0; u < ucount; ++u) {
+    x = point_buffer[3 * u + 0];
+    y = point_buffer[3 * u + 1];
+    z = point_buffer[3 * u + 2];
+    if (x < 0. || x > 1. || y < 0. || y > 1. || z < 0. || z > 1.) {
+      ++ignored;
+      continue;
+    }
+
+    q->x = (p4est_qcoord_t) (x * qlen) << qshift;
+    q->x = SC_MIN (q->x, P4EST_ROOT_LEN - 1);
+    q->y = (p4est_qcoord_t) (y * qlen) << qshift;
+    q->y = SC_MIN (q->y, P4EST_ROOT_LEN - 1);
+#ifdef P4_TO_P8
+    q->z = (p4est_qcoord_t) (z * qlen) << qshift;
+    q->z = SC_MIN (q->z, P4EST_ROOT_LEN - 1);
+#endif
+    q->level = P4EST_MAXLEVEL;
+    q->p.which_tree = 0;        /* TODO: extend this to arbitrary trees */
+    P4EST_ASSERT (p4est_quadrant_is_node (q, true));
+
+    ++q;
+  }
+  P4EST_FREE (point_buffer);
+
+  if (num_points != NULL) {
+    *num_points = (p4est_locidx_t) (ucount - ignored);
+  }
+
+  return points;
+}
+
+int
+main (int argc, char **argv)
+{
+  int                 mpiret;
+  int                 num_procs, rank;
+  int                 maxlevel;
+  char                buffer[BUFSIZ];
+  p4est_locidx_t      num_points;
+  p4est_connectivity_t *conn;
+  p4est_quadrant_t   *points;
+  p4est_t            *p4est;
+  MPI_Comm            mpicomm;
+
+  /* initialize MPI and p4est internals */
+  mpiret = MPI_Init (&argc, &argv);
+  SC_CHECK_MPI (mpiret);
+  mpicomm = MPI_COMM_WORLD;
+  mpiret = MPI_Comm_size (mpicomm, &num_procs);
+  SC_CHECK_MPI (mpiret);
+  mpiret = MPI_Comm_rank (mpicomm, &rank);
+  SC_CHECK_MPI (mpiret);
+
+  sc_init (rank, NULL, NULL, NULL, SC_LP_DEFAULT);
+  p4est_init (NULL, SC_LP_DEFAULT);
+
+  SC_CHECK_ABORT (argc == 2, "Please provide point file prefix");
+  snprintf (buffer, BUFSIZ, "%s%d_%d.pts", argv[1], rank, num_procs);
+  points = read_points (buffer, &num_points);
+  SC_LDEBUGF ("Read %lld points\n", (long long) num_points);
+
+  maxlevel = 5;
+  conn = p4est_connectivity_new_unitsquare ();
+  p4est = p4est_new_points (mpicomm, conn, maxlevel,
+                            points, num_points, 5, NULL, NULL);
+  P4EST_FREE (points);
+
+  p4est_vtk_write_file (p4est, "points");
+
+  p4est_destroy (p4est);
+  p4est_connectivity_destroy (conn);
+
+  /* clean up and exit */
+  sc_finalize ();
+
+  mpiret = MPI_Finalize ();
+  SC_CHECK_MPI (mpiret);
+
+  return 0;
+}
+
+/* EOF points2.c */
