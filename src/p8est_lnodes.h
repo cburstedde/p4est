a9854c1ebb32235a8907d4acec5fbf9b8b754679
Carsten Burstedde <carsten@ices.utexas.edu>
2009-06-26 11:47:30 -0500

    Propose base_5 encoding of hanging faces.

--- src/p8est_lnodes.h
+++ src/p8est_lnodes.h
@@ -40,7 +40,14 @@ SC_EXTERN_C_BEGIN;
  * global_nodes contains the globally unique numbers for independent nodes.
  * Hanging nodes are always local and don't have a global number.
  * They index the geometrically corresponding global indep_node of a neighbor.
- * Whether nodes are hanging or not will be encoded separately.
+ *
+ * Whether nodes are hanging or not is decided based on the element faces.
+ * This information is encoded in face_code with one int16_t per element.
+ * If no faces are hanging, the value is zero, otherwise bit 0x4000 is set
+ * and the hanging status for all 6 faces is encoded in the base_5 system.
+ * Face 0 corresponds to the lowest base_5 digit and so on.  Each base_5 digit
+ * is either 0..3 with the z-order of the hanging face, or 4 if not hanging.
+ * Thus the maximum code is 5^6 - 1 = 15624 < 16384 = 0x4000.
  *
  * Independent nodes can be shared by multiple MPI ranks.
  * The owner rank of a node is the one from the lowest numbered element
@@ -56,12 +63,47 @@ typedef struct p8est_lnodes
   p4est_locidx_t      num_local_elements;
   p4est_locidx_t      num_indep_nodes;
   p4est_locidx_t      owned_offset, num_owned_nodes;
+  int16_t            *face_code;
   p4est_locidx_t     *local_nodes;
   p4est_gloidx_t     *global_nodes;
   int                *sharers_offset;
   int                *sharers;
 }
 p8est_lnodes_t;
+
+/** Decode the face_code into hanging face information.
+ *
+ * This is mostly for demonstration purposes.  Applications probably will
+ * integrate it into their own loop over the face for performance reasons.
+ *
+ * \param[in] face_code as in the p8est_lnodes_t structure.
+ * \return              true if any face is hanging, false otherwise.
+ */
+static inline       bool
+p8est_lnodes_face_decode (int16_t face_code,
+                          bool is_hanging[6], int hanging_face[6])
+{
+  SC_ASSERT (face_code >= 0);
+
+  if (face_code) {
+    int                 i;
+    int                 work = face_code & ~0x4000;
+    div_t               wdiv;
+
+    for (i = 0; i < 6; ++i) {
+      wdiv = div (work, 5);
+      is_hanging[i] = (wdiv.rem != 0x4);
+      hanging_face[i] = wdiv.rem;
+      work = wdiv.quot;
+    }
+    SC_ASSERT (work == 0);
+
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 p8est_lnodes_t     *p8est_lnodes_new (p8est_t * p8est,
                                       sc_array_t * ghost_layer, int degree);
