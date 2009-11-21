4ab15f51f9142b6f7f565d5677994caabf42c4e0
Carsten Burstedde <carsten@ices.utexas.edu>
2009-11-21 11:51:36 -0600

    Whitespace/comments on p4est-setup.

--- doc/p4est-setup.sh
+++ doc/p4est-setup.sh
@@ -12,11 +12,11 @@ INSTALL_FAST="$UNPACK/p4est-install/FAST"
 INSTALL_DEBUG="$UNPACK/p4est-install/DEBUG"
 
 function busage() {
-	echo "Usage: `basename $0` <p4est_tar.gz_file>"
+        echo "Usage: `basename $0` <p4est_tar.gz_file>"
 }
 function bdie () {
-	echo "Error: $@"
-	exit 1
+        echo "Error: $@"
+        exit 1
 }
 
 echo
@@ -27,68 +27,68 @@ echo "Install FAST: $INSTALL_FAST"
 echo "Install DEBUG: $INSTALL_DEBUG"
 echo "Checking environment: CFLAGS P4EST_CFLAGS_FAST P4EST_CFLAGS_DEBUG"
 if test -z "$CFLAGS" -a -z "$P4EST_CFLAGS_FAST" ; then
-	export CFLAGS_FAST="-O2"
+        export CFLAGS_FAST="-O2"
 else
-	export CFLAGS_FAST="$CFLAGS $P4EST_CFLAGS_FAST"
+        export CFLAGS_FAST="$CFLAGS $P4EST_CFLAGS_FAST"
 fi
 echo "CFLAGS_FAST: $CFLAGS_FAST"
 if test -z "$CFLAGS" -a -z "$P4EST_CFLAGS_DEBUG" ; then
-	export CFLAGS_DEBUG="-O0 -g"
+        export CFLAGS_DEBUG="-O0 -g"
 else
-	export CFLAGS_DEBUG="$CFLAGS $P4EST_CFLAGS_DEBUG"
+        export CFLAGS_DEBUG="$CFLAGS $P4EST_CFLAGS_DEBUG"
 fi
 echo "CFLAGS_DEBUG: $CFLAGS_DEBUG"
 
 TGZ="$1"; shift
 if test ! -f "$TGZ" ; then
-	busage
-	bdie "File not found"
+        busage
+        bdie "File not found"
 fi
 if ! (echo "$TGZ" | grep -q 'p4est.*.tar.gz') ; then
-	busage
-	bdie "File name mismatch"
+        busage
+        bdie "File name mismatch"
 fi
 DIR=`echo "$TGZ" | sed 's/\(p4est.*\).tar.gz/\1/'`
 DIR=`basename $DIR`
 echo "Unpack directory: $UNPACK/$DIR"
 if test -d "$UNPACK/$DIR" ; then
-	echo \
-	    "Directory found (remove it and also the build directories" \
-	    "to start over)"
+        echo \
+            "Directory found (remove it and also the build directories" \
+            "to start over)"
 else
-	echo -n "Unpacking... "
-	tar -xvz -f "$TGZ" -C "$UNPACK" >/dev/null
-	echo "done"
+        echo -n "Unpacking... "
+        tar -xvz -f "$TGZ" -C "$UNPACK" >/dev/null
+        echo "done"
 fi
 test -f "$UNPACK/$DIR/src/p4est.h" || bdie "Main header file missing"
 test -f "$UNPACK/$DIR/configure" || bdie "Configure script missing"
 
 echo
 echo "See output in files .../config.output and .../make.output"
-echo "Build FAST version in $BUILD_FAST"
+echo "Build FAST in $BUILD_FAST"
 mkdir -p "$BUILD_FAST"
 cd "$BUILD_FAST"
 "$UNPACK/$DIR/configure" --enable-mpi --without-blas \
-	--prefix="$INSTALL_FAST" CFLAGS="$CFLAGS_FAST" \
-	"$@" > config.output || bdie "Error in configure"
+        --prefix="$INSTALL_FAST" CFLAGS="$CFLAGS_FAST" \
+        "$@" > config.output || bdie "Error in configure"
 make -C sc -j 8 > make.output || bdie "Error in make sc"
 make -j 8 >> make.output || bdie "Error in make p4est"
 make install >> make.output || bdie "Error in make install"
-echo "FAST version installed in $INSTALL_FAST"
+echo "FAST installed in $INSTALL_FAST"
 
 echo
-echo "Build DEBUG version in $BUILD_DEBUG"
+echo "Build DEBUG in $BUILD_DEBUG"
 mkdir -p "$BUILD_DEBUG"
 cd "$BUILD_DEBUG"
 if test -z "$CFLAGS" ; then
-	export CFLAGS="-g -O0"
+        export CFLAGS="-g -O0"
 fi
 "$UNPACK/$DIR/configure" --enable-mpi --without-blas \
         --enable-debug --disable-vtk-binary \
-	--prefix="$INSTALL_DEBUG" CFLAGS="$CFLAGS_DEBUG" \
-	"$@" > config.output || bdie "Error in configure"
+        --prefix="$INSTALL_DEBUG" CFLAGS="$CFLAGS_DEBUG" \
+        "$@" > config.output || bdie "Error in configure"
 make -C sc -j 8 > make.output || bdie "Error in make sc"
 make -j 8 >> make.output || bdie "Error in make p4est"
 make install >> make.output || bdie "Error in make install"
-echo "DEBUG version installed in $INSTALL_DEBUG"
+echo "DEBUG installed in $INSTALL_DEBUG"
 echo
