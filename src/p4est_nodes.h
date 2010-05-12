3c3cf35b6f1e10b704692e35278a883313458ffc
Carsten Burstedde <carsten@ices.utexas.edu>
2010-05-12 17:41:06 -0500

    Use official GPL 2 license stub in src.

--- src/p4est_nodes.h
+++ src/p4est_nodes.h
@@ -1,22 +1,24 @@
 /*
   This file is part of p4est.
-  p4est is a C library to manage a parallel collection of quadtrees and/or
-  octrees.
+  p4est is a C library to manage a collection (a forest) of multiple
+  connected adaptive quadtrees or octrees in parallel.
 
-  Copyright (C) 2007-2009 Carsten Burstedde, Lucas Wilcox.
+  Copyright (C) 2010 The University of Texas System
+  Written by Carsten Burstedde, Lucas C. Wilcox, and Tobin Isaac
 
-  This program is free software: you can redistribute it and/or modify
+  p4est is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
+  the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
+  p4est is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  along with p4est; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
 #ifndef P4EST_NODES_H
