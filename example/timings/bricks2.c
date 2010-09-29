51482e539e8b9225e8da85230fe2b9b9e37122f1
Carsten Burstedde <carsten@ices.utexas.edu>
2010-09-29 11:14:53 -0500

    Start brick timing example and add 2D brick bug

---
+++ example/timings/bricks2.c
@@ -0,0 +1,62 @@
+/*
+  This file is part of p4est.
+  p4est is a C library to manage a collection (a forest) of multiple
+  connected adaptive quadtrees or octrees in parallel.
+
+  Copyright (C) 2010 The University of Texas System
+  Written by Carsten Burstedde, Lucas C. Wilcox, and Tobin Isaac
+
+  p4est is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  p4est is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with p4est; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#ifndef P4_TO_P8
+#include <p4est.h>
+#else
+#include <p8est.h>
+#endif
+
+static void
+run_bricks ()
+{
+#ifndef P4_TO_P8
+  sc_abort_collective ("The brick is not yet implemented in 2D");
+#else
+  p4est_connectivity_t *conn;
+
+  conn = p8est_connectivity_new_brick (1, 1, 1, 0, 0, 0);
+  p4est_connectivity_destroy (conn);
+#endif
+}
+
+int
+main (int argc, char **argv)
+{
+  int                 mpiret;
+
+  mpiret = MPI_Init (&argc, &argv);
+  SC_CHECK_MPI (mpiret);
+
+  sc_init (MPI_COMM_WORLD, 1, 1, NULL, SC_LP_DEFAULT);
+  p4est_init (NULL, SC_LP_DEFAULT);
+
+  run_bricks ();
+
+  sc_finalize ();
+
+  mpiret = MPI_Finalize ();
+  SC_CHECK_MPI (mpiret);
+
+  return 0;
+}
