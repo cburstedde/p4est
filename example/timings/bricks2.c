7d74325683bc9b39929a569bf5423909289ca640
Carsten Burstedde <carsten@ices.utexas.edu>
2010-09-29 13:46:56 -0500

    Prepare brick timings

--- example/timings/bricks2.c
+++ example/timings/bricks2.c
@@ -23,19 +23,112 @@
 
 #ifndef P4_TO_P8
 #include <p4est.h>
+#include <p4est_bits.h>
+#include <p4est_vtk.h>
 #else
 #include <p8est.h>
+#include <p8est_bits.h>
+#include <p8est_vtk.h>
+#endif
+#include <sc_options.h>
+
+/* #define BRICKS_VTK */
+
+#ifdef P4_TO_P8
+
+static int          refine_level;
+static int          level_shift;
+
+static int
+refine_fractal (p4est_t * p4est, p4est_topidx_t which_tree,
+                p4est_quadrant_t * q)
+{
+  int                 qid;
+
+  if ((int) q->level >= refine_level) {
+    return 0;
+  }
+  if ((int) q->level < refine_level - level_shift) {
+    return 1;
+  }
+
+  qid = ((int) q->level == 0 ?
+         (which_tree % P4EST_CHILDREN) : p4est_quadrant_child_id (q));
+
+  return (qid == 0 || qid == 3
+#ifdef P4_TO_P8
+          || qid == 5 || qid == 6
+#endif
+    );
+}
+
 #endif
 
 static void
-run_bricks ()
+run_bricks (MPI_Comm mpicomm, int l, int rlevel)
 {
 #ifndef P4_TO_P8
   sc_abort_collective ("The brick is not yet implemented in 2D");
 #else
+  int                 mpiret;
+  int                 tcount;
+  double              elapsed_create, elapsed_partition, elapsed_balance;
+#ifdef BRICKS_VTK
+  char                filename[BUFSIZ];
+#endif
   p4est_connectivity_t *conn;
+  p4est_t            *p4est;
 
-  conn = p8est_connectivity_new_brick (1, 1, 1, 0, 0, 0);
+  P4EST_GLOBAL_PRODUCTIONF ("Run bricks on level %d/%d\n", l, rlevel);
+  P4EST_ASSERT (l <= rlevel);
+
+  /* create and refine the forest */
+
+  mpiret = MPI_Barrier (mpicomm);
+  SC_CHECK_MPI (mpiret);
+  elapsed_create = -MPI_Wtime ();
+
+  tcount = 1 << l;
+  conn = p8est_connectivity_new_brick (tcount, tcount, tcount, 0, 0, 0);
+  p4est = p4est_new_ext (mpicomm, conn, 0, rlevel - l, 1, 0, NULL, NULL);
+
+  level_shift = 4;
+  refine_level = rlevel - l + level_shift;
+  p4est_refine (p4est, 1, refine_fractal, NULL);
+
+  elapsed_create += MPI_Wtime ();
+
+  /* partition the forest */
+
+  mpiret = MPI_Barrier (mpicomm);
+  SC_CHECK_MPI (mpiret);
+  elapsed_partition = -MPI_Wtime ();
+
+  p4est_partition (p4est, NULL);
+
+  elapsed_partition += MPI_Wtime ();
+
+  /* balance the forest */
+
+  mpiret = MPI_Barrier (mpicomm);
+  SC_CHECK_MPI (mpiret);
+  elapsed_balance = -MPI_Wtime ();
+
+  p4est_balance (p4est, P4EST_BALANCE_FULL, NULL);
+
+  elapsed_balance += MPI_Wtime ();
+
+  /* postprocessing */
+
+  P4EST_GLOBAL_PRODUCTIONF ("Timings %g %g %g\n", elapsed_create,
+                            elapsed_partition, elapsed_balance);
+
+#ifdef BRICKS_VTK
+  snprintf (filename, BUFSIZ, "brick_%02d_%02d_B", rlevel, l);
+  p4est_vtk_write_file (p4est, NULL, filename);
+#endif
+
+  p4est_destroy (p4est);
   p4est_connectivity_destroy (conn);
 #endif
 }
@@ -43,15 +136,32 @@ run_bricks ()
 int
 main (int argc, char **argv)
 {
-  int                 mpiret;
+  MPI_Comm            mpicomm;
+  int                 mpiret, retval;
+  int                 rlevel, l;
+  sc_options_t       *opt;
 
   mpiret = MPI_Init (&argc, &argv);
   SC_CHECK_MPI (mpiret);
+  mpicomm = MPI_COMM_WORLD;
 
   sc_init (MPI_COMM_WORLD, 1, 1, NULL, SC_LP_DEFAULT);
   p4est_init (NULL, SC_LP_DEFAULT);
 
-  run_bricks ();
+  opt = sc_options_new (argv[0]);
+  sc_options_add_int (opt, 'l', "level", &rlevel, 0,
+                      "Upfront refinement level");
+  retval = sc_options_parse (p4est_package_id, SC_LP_ERROR, opt, argc, argv);
+  if (retval == -1 || retval < argc) {
+    sc_options_print_usage (p4est_package_id, SC_LP_PRODUCTION, opt, NULL);
+    sc_abort_collective ("Usage error");
+  }
+
+  for (l = 0; l <= rlevel; ++l) {
+    run_bricks (mpicomm, l, rlevel);
+  }
+
+  sc_options_destroy (opt);
 
   sc_finalize ();
 
