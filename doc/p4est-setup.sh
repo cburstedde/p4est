783f53b1c4f089669bd0a2c7c286005f2b95d578
Carsten Burstedde <carsten@ices.utexas.edu>
2010-10-27 11:13:01 -0500

    Update p4est-setup script

--- doc/p4est-setup.sh
+++ doc/p4est-setup.sh
@@ -4,28 +4,20 @@
 
 # unpack under current directory
 UNPACK=`pwd`
+
 # choose names for fast and debug compilation directories
-BUILD_FAST="$UNPACK/p4est-build/FAST"
-BUILD_DEBUG="$UNPACK/p4est-build/DEBUG"
-# choose names for fast and debug installation directories
-INSTALL_FAST="$UNPACK/p4est-install/FAST"
-INSTALL_DEBUG="$UNPACK/p4est-install/DEBUG"
+BUILD_DIR="$UNPACK/p4est-build"
+BUILD_FAST="$BUILD_DIR/FAST"
+BUILD_DEBUG="$BUILD_DIR/DEBUG"
 
 function busage() {
-        echo "Usage: `basename $0` <p4est_tar.gz_file>"
+        echo "Usage: `basename $0` <p4est_tar.gz_file> [<install location>]"
 }
 function bdie () {
         echo "Error: $@"
         exit 1
 }
 
-echo
-echo "This script tries to unpack, configure and build p4est."
-echo "Build FAST: $BUILD_FAST"
-echo "Build DEBUG: $BUILD_DEBUG"
-echo "Install FAST: $INSTALL_FAST"
-echo "Install DEBUG: $INSTALL_DEBUG"
-echo "Checking environment: CFLAGS P4EST_CFLAGS_FAST P4EST_CFLAGS_DEBUG"
 if test -z "$CFLAGS" -a -z "$P4EST_CFLAGS_FAST" ; then
         export CFLAGS_FAST="-O2"
 else
@@ -48,13 +40,33 @@ if ! (echo "$TGZ" | grep -q 'p4est.*.tar.gz') ; then
         busage
         bdie "File name mismatch"
 fi
+
+# choose names for fast and debug installation directories
+INSTALL_DIR="$1"; shift
+if test -z "$INSTALL_DIR" ; then
+        INSTALL_DIR="$UNPACK/p4est-install"
+fi
+INSTALL_FAST="$INSTALL_DIR/FAST"
+INSTALL_DEBUG="$INSTALL_DIR/DEBUG"
+
+echo
+echo "This script tries to unpack, configure and build the p4est library."
+echo "Build FAST: $BUILD_FAST"
+echo "Build DEBUG: $BUILD_DEBUG"
+echo "Install FAST: $INSTALL_FAST"
+echo "Install DEBUG: $INSTALL_DEBUG"
+echo "Checking environment: CFLAGS P4EST_CFLAGS_FAST P4EST_CFLAGS_DEBUG"
+
+# remove old versions
+if test -d "$BUILD_DIR" ; then
+        rm -rf "$BUILD_DIR"
+fi
+
 DIR=`echo "$TGZ" | sed 's/\(p4est.*\).tar.gz/\1/'`
 DIR=`basename $DIR`
 echo "Unpack directory: $UNPACK/$DIR"
 if test -d "$UNPACK/$DIR" ; then
-        echo \
-            "Directory found (remove it and also the build directories" \
-            "to start over)"
+        echo "Source directory found (remove it to unpack anew)"
 else
         echo -n "Unpacking... "
         tar -xvz -f "$TGZ" -C "$UNPACK" >/dev/null
@@ -63,30 +75,32 @@ fi
 test -f "$UNPACK/$DIR/src/p4est.h" || bdie "Main header file missing"
 test -f "$UNPACK/$DIR/configure" || bdie "Configure script missing"
 
-echo
 echo "See output in files .../config.output and .../make.output"
-echo "Build FAST in $BUILD_FAST"
+echo
+echo "Build FAST version in $BUILD_FAST"
 mkdir -p "$BUILD_FAST"
 cd "$BUILD_FAST"
-"$UNPACK/$DIR/configure" --enable-mpi --without-blas \
+"$UNPACK/$DIR/configure" --enable-mpi --enable-shared \
+        --disable-vtk-binary --without-blas \
         --prefix="$INSTALL_FAST" CFLAGS="$CFLAGS_FAST" \
         CPPFLAGS="-DSC_LOG_PRIORITY=SC_LP_ESSENTIAL" \
         "$@" > config.output || bdie "Error in configure"
 make -C sc -j 8 > make.output || bdie "Error in make sc"
 make -j 8 >> make.output || bdie "Error in make p4est"
 make install >> make.output || bdie "Error in make install"
-echo "FAST installed in $INSTALL_FAST"
+echo "FAST version installed in $INSTALL_FAST"
 
 echo
-echo "Build DEBUG in $BUILD_DEBUG"
+echo "Build DEBUG version in $BUILD_DEBUG"
 mkdir -p "$BUILD_DEBUG"
 cd "$BUILD_DEBUG"
-"$UNPACK/$DIR/configure" --enable-mpi --without-blas \
-        --enable-debug --disable-vtk-binary \
+"$UNPACK/$DIR/configure" --enable-debug --enable-mpi --enable-shared \
+        --disable-vtk-binary --without-blas \
         --prefix="$INSTALL_DEBUG" CFLAGS="$CFLAGS_DEBUG" \
+        CPPFLAGS="-DSC_LOG_PRIORITY=SC_LP_ESSENTIAL" \
         "$@" > config.output || bdie "Error in configure"
 make -C sc -j 8 > make.output || bdie "Error in make sc"
 make -j 8 >> make.output || bdie "Error in make p4est"
 make install >> make.output || bdie "Error in make install"
-echo "DEBUG installed in $INSTALL_DEBUG"
+echo "DEBUG version installed in $INSTALL_DEBUG"
 echo
