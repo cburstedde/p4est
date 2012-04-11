cdfc1bf5addc41e99fa3537411f802fea441b49e
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2012-04-11 18:10:21 +0200

    Write simple connectivity read program

---
+++ example/tetgen/read_conn2.c
@@ -0,0 +1,48 @@
+/*
+  This file is part of p4est.
+  p4est is a C library to manage a collection (a forest) of multiple
+  connected adaptive quadtrees or octrees in parallel.
+
+  Copyright (C) 2012 Carsten Burstedde
+
+  p4est is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  p4est is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with p4est; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#ifndef P4_TO_P8
+#include <p4est_connectivity.h>
+#else
+#include <p8est_connectivity.h>
+#endif
+
+int
+main (int argc, char **argv)
+{
+  p4est_connectivity_t * conn;
+
+  if (argc != 2) {
+    char               *cp, *bn;
+
+    cp = strdup (argv[0]);
+    bn = basename (cp);
+    fprintf (stderr, "Usage: %s <connectivity file>\n", bn);
+    free (cp);
+    exit (1);
+  }
+
+  conn = p4est_connectivity_load (argv[1], NULL);
+  p4est_connectivity_destroy (conn);
+
+  return 0;
+}
