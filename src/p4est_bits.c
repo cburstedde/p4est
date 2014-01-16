60780c6f21bffff0dd3e455c08b115b60d03eb90
Toby Isaac <tisaac@ices.utexas.edu>
2014-01-15 21:37:00 -0600

    bits: fix type of tree argument in face_neighbor_extra

--- src/p4est_bits.c
+++ src/p4est_bits.c
@@ -832,7 +832,7 @@ p4est_quadrant_face_neighbor (const p4est_quadrant_t * q,
 
 p4est_locidx_t
 p4est_quadrant_face_neighbor_extra (const p4est_quadrant_t * q,
-                                    p4est_locidx_t t, int face,
+                                    p4est_topidx_t t, int face,
                                     p4est_quadrant_t * r, int *nface,
                                     p4est_connectivity_t * conn)
 {
--- src/p4est_bits.h
+++ src/p4est_bits.h
@@ -340,7 +340,7 @@ void                p4est_quadrant_face_neighbor (const p4est_quadrant_t * q,
  * tree across \face, then -1 is returned.
  */
 p4est_locidx_t      p4est_quadrant_face_neighbor_extra (const p4est_quadrant_t
-                                                        * q, p4est_locidx_t t,
+                                                        * q, p4est_topidx_t t,
                                                         int face,
                                                         p4est_quadrant_t * r,
                                                         int *nface,
