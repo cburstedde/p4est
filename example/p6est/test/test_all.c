0c3bf042c68b49c8fd1a5dbfa58f32a42b5c891c
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2014-02-18 21:48:55 +0100

    Add option to choose save filename

--- example/p6est/test/test_all.c
+++ example/p6est/test/test_all.c
@@ -174,6 +174,7 @@ main (int argc, char **argv)
   sc_options_t       *opt;
   int                 first_argc;
   const char         *config_name;
+  const char         *save_filename;
   sc_statinfo_t       stats[TIMINGS_NUM_STATS];
   sc_flopinfo_t       fi, snapshot;
   int                 mpiret;
@@ -194,6 +195,8 @@ main (int argc, char **argv)
                       "initial refine level");
   sc_options_add_string (opt, 'c', "configuration", &config_name, "unit",
                          "configuration: brick23|corner|cubed|disc|moebius|periodic|pillow|rotwrap|star|unit");
+  sc_options_add_string (opt, 'P', "save-file", &save_filename,
+                         "p6est_test_all.p6p", "filename for saving");
   sc_options_add_switch (opt, 'w', "write-vtk", &vtk, "write vtk files");
 
   first_argc = sc_options_parse (p4est_package_id, SC_LP_DEFAULT,
@@ -377,12 +380,12 @@ main (int argc, char **argv)
   P4EST_GLOBAL_PRODUCTIONF ("p6est checksum 0x%08x\n", crc_computed);
 
   sc_flops_snap (&fi, &snapshot);
-  p6est_save ("p6est_test_all.p6p", p6est, 1);
+  p6est_save (save_filename, p6est, 1);
   sc_flops_shot (&fi, &snapshot);
   sc_stats_set1 (&stats[TIMINGS_SAVE], snapshot.iwtime, "Save");
 
   sc_flops_snap (&fi, &snapshot);
-  copy_p6est = p6est_load ("p6est_test_all.p6p", p6est->mpicomm,
+  copy_p6est = p6est_load (save_filename, p6est->mpicomm,
                            p6est->data_size, 1, p6est->user_pointer,
                            &copy_conn);
   sc_flops_shot (&fi, &snapshot);
