39aae24410609969c6c69efc7f5c166fe68e4f64
Toby Isaac <tisaac@ices.utexas.edu>
2014-03-22 16:41:54 -0500

    perfscript.sh revisions

--- example/timings/perfscript.sh
+++ example/timings/perfscript.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 command -v perf >/dev/null 2>&1 || { echo >&2 "Could not find perf.  Aborting."; exit 1; }
 
-for test in L1-dcache-load-misses L1-dcache-store-misses LLC-load-misses LLC-store-misses LLC-prefetch-misses dTLB-load-misses dTLB-store-misses dTLB-prefetch-misses iTLB-load-misses branch-load-misses; do
-  for type in nodes lnodes base; do
+STATS="instructions,cache-misses,branch-misses"
+REPCOUNT=30
+
+SPACESTATS=`echo $STATS | sed 's/,/ /g'`
+echo "type level time $SPACESTATS" > p8.perf
+
+for i in 4 5 6 7 8; do
+  for type in "base" "nodes" "lnodes"; do
     if [ $type = "nodes" ]; then
       othertype="lnodes"
     elif [ $type = "base" ]; then
@@ -11,9 +17,28 @@ for test in L1-dcache-load-misses L1-dcache-store-misses LLC-load-misses LLC-sto
     else
       othertype="nodes"
     fi
-    for i in 0 1 2 3 4 5 6 7; do
-      perf stat --event=$test ./p8est_timings -c unit --skip-$othertype --level $i 2> p8.$test.$type.$i > /dev/null
+    perf stat -o p8.$type.$i -e "$STATS" -r $REPCOUNT ./p8est_timings \
+      -c unit --skip-$othertype --level $i > p8.$type.$i.stdout
+    runtime= \
+      `egrep "[0-9.]+ seconds time elapsed" p8.$type.$i | awk '{print $1;}'`
+    accum=$runtime
+    for stat in $SPACESTATS; do
+      count= \
+        `egrep "[0-9,]+ $stat" p8.$type.$i | awk '{print $1;}' | sed 's/,//g'`
+      accum="$accum $count"
     done
+    if [ $type = "base" ]; then
+      baseaccum=$accum
+    else
+      read -a basearray <<< "$baseaccum"
+      read -a array <<< "$accum"
+      for j in "${!array[@]}"; do
+        val=`echo "${array[j]}-${basearray[j]}" | bc -l`
+        array[j]=$val
+      done
+      diff="${array[*]}"
+      echo "$type $i $diff" >> p8.perf
+    fi
   done
 done
 
