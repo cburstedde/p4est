a0838b8bdbcc431bcc87abb566433e68e52bff50
Toby Isaac <tisaac@ices.utexas.edu>
2014-04-08 08:00:00 -0500

    p6est: document p6est_new_from_p4est

--- example/p6est/p6est.h
+++ example/p6est/p6est.h
@@ -221,6 +221,22 @@ p6est_t            *p6est_new (MPI_Comm mpicomm,
                                size_t data_size,
                                p6est_init_t init_fn, void *user_pointer);
 
+/** Create a new forest from an already created p4est that represents
+ * columns.
+ *
+ * \param [in] p4est         A valid p4est.  A deep copy will be created, so
+ *                           this can be destroyed without affectin the new
+ *                           p6est object.
+ * \param [in] top_vertices  the same as in p6est_conectivity_new
+ * \param [in] height        the same as in p6est_conectivity_new
+ * \param [in] min_zlevel    the same as in p6est_new
+ * \param [in] data_size     the same as in p6est_new
+ * \param [in] init_fn       the same as in p6est_new
+ * \param [in] user_pointer  the same as in p6est_new
+ *
+ * \return This returns a valid forest.  The user must destroy the
+ * connectivity for the new p6est independently.
+ */
 p6est_t            *p6est_new_from_p4est (p4est_t * p4est,
                                           double *top_vertices,
                                           double height[3], int min_zlevel,
