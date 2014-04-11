4f16eda37d6c5bd832d9514f5d395c6db53af4ec
Johannes Holke <holke@ins.uni-bonn.de>
2014-04-11 13:20:27 +0200

    Added a comment.

--- src/p8est_ghost.h
+++ src/p8est_ghost.h
@@ -68,7 +68,17 @@ typedef struct
 }
 p8est_ghost_t;
 
-
+/** Examine if a ghost structure is valid as desribed above.
+ * Test if within a ghost-structure the arrays ghosts and mirrors are in
+ * p4est_quadrant_compare_piggy order.
+ * Test if local_num in piggy3 data member of the quadrants in ghosts and
+ * mirrors are in ascending order (ascending within each rank for ghost).
+ *
+ * Test if the p4est_locidx_t arrays are in ascending order
+ * (for mirror_proc_mirrors ascending within each rank)
+ * \param [in] ghost    Ghost layer structure.
+ * \return true if \a ghost is valid
+ */
 int                 p8est_ghost_is_valid (p8est_ghost_t *ghost);
 
 
