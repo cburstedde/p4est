67e5f24d1e56962d723efc1f77e6dcd196ec4875
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2014-06-28 17:19:54 +0200

    Start example/steps/p4est_step1

---
+++ example/steps/p4est_step1.c
@@ -0,0 +1,111 @@
+/*
+  This file is part of p4est.
+  p4est is a C library to manage a collection (a forest) of multiple
+  connected adaptive quadtrees or octrees in parallel.
+
+  Copyright (C) 2010 The University of Texas System
+  Written by Carsten Burstedde, Lucas C. Wilcox, and Tobin Isaac
+
+  p4est is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  p4est is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with p4est; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/* p4est has two separate interfaces for 2D and 3D, p4est*.h and p8est*.h.
+ * Most API functions are available for both dimensions.  The header file
+ * p4est_to_p8est.h #define's the 2D names to the 3D names such that most code
+ * only needs to be written once.  In this example, we rely on this. */
+#ifndef P4_TO_P8
+/* #include <p4est_bits.h> */
+#include <p4est_vtk.h>
+#else
+#include <p8est_vtk.h>
+#endif
+
+static int
+refine_fn (p4est_t * p4est, p4est_topidx_t which_tree,
+           p4est_quadrant_t * quadrant)
+{
+  /* The connectivity chosen in main () only consists of one tree. */
+  P4EST_ASSERT (which_tree == 0);
+
+  return 0;
+}
+
+int
+main (int argc, char **argv)
+{
+  int                 mpiret;
+  int                 recursive, partforcoarsen;
+  sc_MPI_Comm         mpicomm;
+  p4est_t            *p4est;
+  p4est_connectivity_t *conn;
+
+  /* Initialize MPI; see sc_mpi.h.
+   * If configure --enable-mpi is given these are true MPI calls.
+   * Else these are dummy functions that simulate a single-processor run. */
+  mpiret = sc_MPI_Init (&argc, &argv);
+  SC_CHECK_MPI (mpiret);
+  mpicomm = sc_MPI_COMM_WORLD;
+
+  /* These functions are optional.  If called they store the MPI rank as a
+   * static variable so subsequent global p4est log messages are only issued
+   * from processor zero.  Here we turn off most of the logging; see sc.h. */
+  sc_init (mpicomm, 1, 1, NULL, SC_LP_ESSENTIAL);
+  p4est_init (NULL, SC_LP_PRODUCTION);
+  P4EST_GLOBAL_PRODUCTIONF
+    ("This is the p4est %dD demo example/steps/%s_step1\n",
+     P4EST_DIM, P4EST_STRING);
+
+  /* Create a forest that consists of just one quadtree/octree.
+   * This file is compiled for both 2D and 3D: the macro P4_TO_P8 can be
+   * checked to execute dimension-dependent code. */
+#ifndef P4_TO_P8
+  conn = p4est_connectivity_new_unitsquare ();
+#else
+  conn = p8est_connectivity_new_unitcube ();
+#endif
+
+  /* Create a forest that is not refined; it consists of the root octant. */
+  p4est = p4est_new (mpicomm, conn, 0, NULL, NULL);
+
+  /* Refine the forest recursively in parallel.
+   * Since refinement does not change the partition boundary, this call
+   * must not create an overly large number of quadrants.  A numerical
+   * application would call p4est_refine non-recursively in a loop,
+   * repartitioning in each iteration. */
+  recursive = 1;
+  p4est_refine (p4est, recursive, refine_fn, NULL);
+
+  /* Partition: The quadrants are redistributed for equal element count.  The
+   * partition can optionally be modified such that a family of octants, which
+   * are possibly ready for coarsening, are never split between processors. */
+  partforcoarsen = 0;
+  p4est_partition (p4est, partforcoarsen, NULL);
+
+  /* Write the forest to disk for visualization, one file per processor. */
+  p4est_vtk_write_file (p4est, NULL, "step1");
+
+  /* Destroy the p4est and the connectivity structure. */
+  p4est_destroy (p4est);
+  p4est_connectivity_destroy (conn);
+
+  /* Verify that allocations internal to p4est and sc do not leak memory.
+   * This should be called if sc_init () has been called earlier. */
+  sc_finalize ();
+
+  /* This is standard MPI programs.  Without --enable-mpi, this is a dummy. */
+  mpiret = sc_MPI_Finalize ();
+  SC_CHECK_MPI (mpiret);
+  return 0;
+}
