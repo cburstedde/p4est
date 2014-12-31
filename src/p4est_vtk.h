861bb9ceffd1b8c6cfe6794bdbb8e59931e2dc70
Ethan Alan Hereth <e.a.hereth@gmail.com>
2014-12-31 16:41:20 -0500

    Document p{4,8}est_vtk_write_{cell,point}_datav
    This commit simply adds Doxygen documentation for both of the datav
    functions.
    
    NOTE: if p4est_vtk_write_all is to be eliminated, I am not sure these
    functions will be needed.

--- src/p4est_vtk.h
+++ src/p4est_vtk.h
@@ -242,14 +242,51 @@ p4est_vtk_context_t *p4est_vtk_write_point_data (p4est_vtk_context_t * cont,
                                                  int num_point_scalars,
                                                  int num_point_vectors, ...);
 
-/** TODO: Please document this function and
- *        add analogous function for cell data.
+/** Write VTK point data.
+ *
+ * This function exports custom point data to the vtk file; it is functionally
+ * the same as \b p4est_vtk_write_point_data with the only difference being
+ * that instead of a variable argument list, an initialized \a va_list is
+ * passed as the last argument. The \a va_list is initialized from the variable
+ * argument list of the calling function.
+ *
+ * \note This function is actually called from \b p4est_vtk_write_point_data
+ * and does all of the work.
+ *
+ * \param [in,out] cont    A vtk context created by p4est_vtk_write_header.
+ * \param [in] num_point_scalars Number of point scalar datasets to output.
+ * \param [in] num_point_vectors Number of point vector datasets to output.
+ * \param [in,out] ap      An initialized va_list used to access the
+ *                         scalar/vector data.
+ *
+ * \return          On success, the context that has been passed in.
+ *                  On failure, returns NULL and deallocates the context.
  */
 p4est_vtk_context_t *p4est_vtk_write_point_datav (p4est_vtk_context_t * cont,
                                                   int num_point_scalars,
                                                   int num_point_vectors,
                                                   va_list ap);
 
+/** Write VTK cell data.
+ *
+ * This function exports custom cell data to the vtk file; it is functionally
+ * the same as \b p4est_vtk_write_cell_data with the only difference being
+ * that instead of a variable argument list, an initialized \a va_list is
+ * passed as the last argument. The \a va_list is initialized from the variable
+ * argument list of the calling function.
+ *
+ * \note This function is actually called from \b p4est_vtk_write_cell_data
+ * and does all of the work.
+ *
+ * \param [in,out] cont    A vtk context created by p4est_vtk_write_header.
+ * \param [in] num_point_scalars Number of point scalar datasets to output.
+ * \param [in] num_point_vectors Number of point vector datasets to output.
+ * \param [in,out] ap      An initialized va_list used to access the
+ *                         scalar/vector data.
+ *
+ * \return          On success, the context that has been passed in.
+ *                  On failure, returns NULL and deallocates the context.
+ */
 p4est_vtk_context_t *p4est_vtk_write_cell_datav (p4est_vtk_context_t * cont,
                                                  int write_tree,
                                                  int write_level,
