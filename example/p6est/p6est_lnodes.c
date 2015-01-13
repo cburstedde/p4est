c3e8691c528b6f999fdeac84a3750da588e6f6ba
Toby Isaac <tisaac@ices.utexas.edu>
2015-01-13 11:31:05 -0600

    off by one

--- example/p6est/p6est_lnodes.c
+++ example/p6est/p6est_lnodes.c
@@ -429,7 +429,7 @@ p6est_lnodes_get_column_labels (p6est_t * p6est, p8est_lnodes_t * lnodes)
       for (i = 0; i < vnodes; i += stride) {
         p4est_locidx_t      fnid = lnodes->element_nodes[vnodes * lfirst + i];
         p4est_locidx_t      lnid =
-          lnodes->element_nodes[vnodes * llast + i + (stride - 1)];
+          lnodes->element_nodes[vnodes * (llast - 1) + i + (stride - 1)];
         P4EST_ASSERT (lnid >= 0);
         P4EST_ASSERT (lnid >= fnid);
         P4EST_ASSERT (fnid < lnodes->num_local_nodes);
