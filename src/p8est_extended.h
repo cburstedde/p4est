68b6b6ab684cf0f55ae3fdf060bb5be37a9c8ed3
Johann Rudi <johann@ices.utexas.edu>
2015-02-12 21:08:03 -0600

    p8est_extended: Minor change.

--- src/p8est_extended.h
+++ src/p8est_extended.h
@@ -166,7 +166,7 @@ p4est_gloidx_t      p8est_partition_ext (p8est_t * p8est,
  * \param [in,out] num_quadrants_in_proc partition that will be corrected
  * \return                               absolute number of moved quadrants
  */
-p4est_locidx_t      p8est_partition_for_coarsening (p8est_t * p4est,
+p4est_locidx_t      p8est_partition_for_coarsening (p8est_t * p8est,
                                                     p4est_locidx_t *
                                                     num_quadrants_in_proc);
 
