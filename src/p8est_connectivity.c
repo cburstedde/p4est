85577a0fedea33c901b79b3622e13ee9ba6666c2
Michael Lahnert <michael.lahnert@ipvs.uni-stuttgart.de>
2016-06-21 14:43:06 +0200

    bugfix

--- src/p8est_connectivity.c
+++ src/p8est_connectivity.c
@@ -71,7 +71,7 @@ const int           p8est_face_edge_permutations[8][4] =
  { 2, 3, 1, 0 },
  { 3, 2, 0, 1 },
  { 3, 2, 1, 0 }};
-const int           p8est_face_edge_permutation_sets[2][4] =
+const int           p8est_face_edge_permutation_sets[3][4] =
 {{ 4, 1, 2, 7 },
  { 0, 6, 5, 3 },
  { 0, 5, 6, 3 }};
