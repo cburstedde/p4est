bf6d29298567f85b64c7cb77394ee345e190eda8
Michael Lahnert <michael.lahnert@ipvs.uni-stuttgart.de>
2016-06-21 20:23:04 +0200

    fix documentation, make 2 trees available in p8est

--- src/p8est_connectivity.h
+++ src/p8est_connectivity.h
@@ -507,6 +507,17 @@ p8est_connectivity_t *p8est_connectivity_new_rotwrap (void);
  */
 p8est_connectivity_t *p8est_connectivity_new_twocubes (void);
 
+/** Create a connectivity structure for two trees being rotated
+ * w.r.t. each other in a user-defined way.
+ * \param[in] l_face      index of left face
+ * \param[in] r_face      index of right face
+ * \param[in] orientation orientation of trees w.r.t. each other
+ */
+p8est_connectivity_t *p8est_connectivity_new_twotrees (p4est_topidx_t l_face,
+                                                       p4est_topidx_t r_face,
+                                                       p4est_topidx_t
+                                                       orientation);
+
 /** Create a connectivity structure that contains two cubes
  * where the two far ends are identified periodically.
  */
