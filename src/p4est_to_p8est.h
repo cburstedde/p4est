ee9307f170d264213efef7bc7168e2da06bbf262
Michael Lahnert <michael.lahnert@ipvs.uni-stuttgart.de>
2017-06-12 09:34:20 +0200

    use p4est_to_p8est for connectivity_new_periodic

--- src/p4est_to_p8est.h
+++ src/p4est_to_p8est.h
@@ -134,6 +134,7 @@
 #define p4est_connectivity_memory_used  p8est_connectivity_memory_used
 #define p4est_connectivity_new          p8est_connectivity_new
 #define p4est_connectivity_new_brick    p8est_connectivity_new_brick
+#define p4est_connectivity_new_periodic p8est_connectivity_new_periodic
 #define p4est_connectivity_new_twotrees p8est_connectivity_new_twotrees
 #define p4est_connectivity_new_byname   p8est_connectivity_new_byname
 #define p4est_connectivity_new_copy     p8est_connectivity_new_copy
