41b7c79869e0417990797ea6b2aacd257bffa665
tgriesbach <44554359+tgriesbach@users.noreply.github.com>
2018-11-01 11:32:41 +0100

    Add this file to the doxygen documentation

--- src/p4est_algorithms.h
+++ src/p4est_algorithms.h
@@ -22,6 +22,13 @@
   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
+/** \file p4est_algorithms.h
+ *
+ * Routines for managing quadrants.
+ *
+ * \ingroup p4est
+ */
+
 #ifndef P4EST_ALGORITHMS_H
 #define P4EST_ALGORITHMS_H
 
