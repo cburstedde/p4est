c84bd5ecd599bf413d890a50776b9325ecdca385
tgriesbach <44554359+tgriesbach@users.noreply.github.com>
2018-12-20 11:47:02 +0100

    Update the documentation of p4est_find_partition

--- src/p4est_search.h
+++ src/p4est_search.h
@@ -37,15 +37,18 @@
 SC_EXTERN_C_BEGIN;
 
 /** Binary search in partition array.
- * Given targets my_begin, my_end with my_begin <= my_end, find offsets such
- * that search_in[begin] >= my_begin and my_begin < search_in[end] >= my_end.
+ * Given two targets my_begin and my_end with my_begin <= my_end, find offsets such that
+ * search_in[begin] >= my_begin, my_begin < search_in[end] and my_end <= search_in[end].
+ * If more than one index satisfy the condition, then the minimal index is the result.
+ * If there is no index that satisfies the condition, then begin and end are equal.
  * \param [in] num_procs    Number of processes to get the length of search_in.
- * \param [in] search_in    The sorted array in that the function will search.
+ * \param [in] search_in    The sorted array (ascending) in that the function will search.
+ *                          If k indexes search_in, then 0 <= k < num_procs.
  * \param [in] my_begin     The first target that defines the start of the search window.
  * \param [in] my_end       The second target that defines the end of the search window.
  * \param [in,out] begin    The first offset such that search_in[begin] >= my_begin.
  * \param [in,out] end      The second offset such that
- *                          my_begin < search_in[end] >= my_end.
+ *                          my_begin < search_in[end] and my_end <= search_in[end].
  */
 void                p4est_find_partition (const int num_procs,
                                           p4est_gloidx_t * search_in,
