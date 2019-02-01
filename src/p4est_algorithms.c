eeee60aa1da860fccbe3b4e29a69750d14c28a0a
Tim Griesbach <tim.griesbach@uni-bonn.de>
2019-02-01 03:47:56 +0100

    Some debug routines; just for test purpose

--- src/p4est_algorithms.c
+++ src/p4est_algorithms.c
@@ -2875,18 +2875,19 @@ p4est_partition_given (p4est_t * p4est,
 
   p4est_find_partition (num_procs, global_last_quad_index,
                         my_begin, my_end, &from_begin, &from_end);
-
-  for (from_proc = from_begin; from_proc <= from_end; ++from_proc) {
-    lower_bound =
-      (from_proc == 0) ? 0 : (global_last_quad_index[from_proc - 1] + 1);
-    num_recv_from[from_proc] =
-      SC_MIN (my_end,
-              global_last_quad_index[from_proc]) - SC_MAX (my_begin,
-                                                           lower_bound)
-      + 1;
-    P4EST_ASSERT (num_recv_from[from_proc] >= 0);
-    if (from_proc != rank)
-      ++num_proc_recv_from;
+  if (my_begin <= my_end) {
+    for (from_proc = from_begin; from_proc <= from_end; ++from_proc) {
+      lower_bound =
+        (from_proc == 0) ? 0 : (global_last_quad_index[from_proc - 1] + 1);
+      num_recv_from[from_proc] =
+        SC_MIN (my_end,
+                global_last_quad_index[from_proc]) - SC_MAX (my_begin,
+                                                             lower_bound)
+        + 1;
+      P4EST_ASSERT (num_recv_from[from_proc] >= 0);
+      if (from_proc != rank)
+        ++num_proc_recv_from;
+    }
   }
 
   from_begin_global_quad = from_begin;
@@ -2917,6 +2918,10 @@ p4est_partition_given (p4est_t * p4est,
       }
     }
 
+    printf
+      ("Debug[%i]: num_proc_recv_from = %i \t old_num_proc_recv_from = %i \t new_global_last_quad_index[0] = %li\n",
+       rank, num_proc_recv_from, old_num_proc_recv_from,
+       new_global_last_quad_index[0]);
     P4EST_ASSERT (num_proc_recv_from == old_num_proc_recv_from);
     for (i = 0; i < num_procs; ++i) {
       if (num_recv_from[i] != 0) {
@@ -2981,21 +2986,22 @@ p4est_partition_given (p4est_t * p4est,
 
   p4est_find_partition (num_procs, new_global_last_quad_index,
                         my_begin, my_end, &to_begin, &to_end);
-
-  for (to_proc = to_begin; to_proc <= to_end; ++to_proc) {
-    /* I send to to_proc which may be empty */
-    lower_bound =
-      (to_proc == 0) ? 0 : (new_global_last_quad_index[to_proc - 1] + 1);
-    num_send_to[to_proc] =
-      SC_MIN (my_end,
-              new_global_last_quad_index[to_proc]) - SC_MAX (my_begin,
-                                                             lower_bound)
-      + 1;
-    begin_send_to[to_proc] =
-      (num_send_to[to_proc] == 0) ? -1 : SC_MAX (my_begin, lower_bound);
-    P4EST_ASSERT (num_send_to[to_proc] >= 0);
-    if (to_proc != rank)
-      ++num_proc_send_to;
+  if (my_begin <= my_end) {
+    for (to_proc = to_begin; to_proc <= to_end; ++to_proc) {
+      /* I send to to_proc which may be empty */
+      lower_bound =
+        (to_proc == 0) ? 0 : (new_global_last_quad_index[to_proc - 1] + 1);
+      num_send_to[to_proc] =
+        SC_MIN (my_end,
+                new_global_last_quad_index[to_proc]) - SC_MAX (my_begin,
+                                                               lower_bound)
+        + 1;
+      begin_send_to[to_proc] =
+        (num_send_to[to_proc] == 0) ? -1 : SC_MAX (my_begin, lower_bound);
+      P4EST_ASSERT (num_send_to[to_proc] >= 0);
+      if (to_proc != rank)
+        ++num_proc_send_to;
+    }
   }
 
 #ifdef P4EST_ENABLE_DEBUG
