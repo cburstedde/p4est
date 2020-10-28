4d06fa8118d7d8d2edf59c61daeaa839e777b8c3
Tim Griesbach <tim.griesbach@uni-bonn.de>
2020-10-28 16:51:35 +0100

    Simplify predecessor

--- src/p4est_bits.c
+++ src/p4est_bits.c
@@ -2332,7 +2332,7 @@ p4est_quadrant_predecessor (const p4est_quadrant_t * quadrant,
   int                 level;
   int                 predecessor_id;
   int                 quadrant_length, mask;
-  int                 shift;
+  int                 shift_last_descendant, shift_combined;
 
   P4EST_ASSERT (p4est_quadrant_is_extended (quadrant));
   P4EST_ASSERT (quadrant->level > 0);
@@ -2350,23 +2350,25 @@ p4est_quadrant_predecessor (const p4est_quadrant_t * quadrant,
   /* compute result */
   if (level < quadrant->level) {
     quadrant_length = P4EST_QUADRANT_LEN (level);
-    shift = quadrant_length - P4EST_QUADRANT_LEN (quadrant->level);
+    shift_last_descendant =
+      quadrant_length - P4EST_QUADRANT_LEN (quadrant->level);
+    shift_combined = shift_last_descendant + quadrant_length;
     mask = ~(P4EST_QUADRANT_LEN (level - 1) - 1);
 
-    /* coarsen to level - 1 and add shifts according to the predecessor_id */
+    /** Coarsen to level - 1 and add shifts according to the predecessor_id
+     *  and determine the last descendant of the resulting quadrant 
+     *  of level ´level´ on level quadrant->level.
+     */
     result->x =
       (predecessor_id & 0x01) ? (quadrant->x & mask) +
-      quadrant_length : quadrant->x & mask;
-    result->x += shift;
+      shift_combined : (quadrant->x & mask) + shift_last_descendant;
     result->y =
       (predecessor_id & 0x02) ? (quadrant->y & mask) +
-      quadrant_length : quadrant->y & mask;
-    result->y += shift;
+      shift_combined : (quadrant->y & mask) + shift_last_descendant;
 #ifdef P4_TO_P8
     result->z =
       (predecessor_id & 0x04) ? (quadrant->z & mask) +
-      quadrant_length : quadrant->z & mask;
-    result->z += shift;
+      shift_combined : (quadrant->z & mask) + shift_last_descendant;
 #endif
     result->level = quadrant->level;
   }
