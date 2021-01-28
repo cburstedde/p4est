c63f7edc0ab66cf64f6646fccb43068117b22464
Michael Hirsch <scivision@users.noreply.github.com>
2021-01-28 02:10:56 -0500

    accomodate CMake >= 3.13

--- cmake/sc.cmake
+++ cmake/sc.cmake
@@ -8,4 +8,10 @@ FetchContent_Declare(SC
   GIT_REPOSITORY https://github.com/scivision/libsc.git
   GIT_TAG prev3-develop
   CMAKE_ARGS -Dmpi=${mpi})
-FetchContent_MakeAvailable(SC)
+
+if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.14)
+  FetchContent_MakeAvailable(SC)
+elseif(NOT sc_POPULATED)
+  FetchContent_Populate(SC)
+  add_subdirectory(${sc_SOURCE_DIR} ${sc_BINARY_DIR})
+endif()
