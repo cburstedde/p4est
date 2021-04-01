44ac69cb14d7840a0321bc15bfd5dfbd96e9aa88
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2021-04-01 16:32:35 +0200

    cmake: mpi and openmpi are disabled by default

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -3,9 +3,6 @@ option(enable_p8est "build p8est" on)
 
 option(vtk "VTK interface" on)
 
-option(mpi "use MPI library" on)
-option(openmp "use OpenMP" on)
-
 option(sc_external "force build of libsc")
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS on)
