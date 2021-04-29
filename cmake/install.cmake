c4e6610729fb6dc866644c014a8f2c69d75dcc14
Michael Hirsch <scivision@users.noreply.github.com>
2021-04-28 23:48:07 -0400

    cmake: remove unused parameters

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -8,15 +8,11 @@ configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in
 
 write_basic_package_version_file(
   ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake
-  VERSION ${${PROJECT_NAME}_VERSION}
-  COMPATIBILITY SameMinorVersion
-)
+  COMPATIBILITY SameMinorVersion)
 
 install(EXPORT ${PROJECT_NAME}Targets
-  FILE ${PROJECT_NAME}Targets.cmake
   NAMESPACE ${PROJECT_NAME}::
-  DESTINATION lib/cmake/${PROJECT_NAME}
-   )
+  DESTINATION lib/cmake/${PROJECT_NAME})
 
 install(FILES
   ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake
@@ -25,8 +21,7 @@ install(FILES
 
 export(EXPORT ${PROJECT_NAME}Targets
   FILE ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}Targets.cmake
-  NAMESPACE ${PROJECT_NAME}::
-)
+  NAMESPACE ${PROJECT_NAME}::)
 
 # --- CPack
 
