fce394da7e3477dd33bb3efd7b6f9567edab0835
Bruno Turcksin <bruno.turcksin@gmail.com>
2022-01-06 21:15:46 +0000

    Fix review comments

--- cmake/git.cmake
+++ cmake/git.cmake
@@ -1,10 +1,13 @@
 # --- extract version from Git
 
+set(PROJECT_MAJOR 0)
+set(PROJECT_MINOR 0)
+set(PROJECT_PATCH 0)
 set(PROJECT_VERSION 0.0.0)
 find_program(GIT_VERSION_GEN NAMES git-version-gen
              PATHS ${CMAKE_SOURCE_DIR}/build-aux NO_DEFAULT_PATH)
 if(GIT_VERSION_GEN)
-  execute_process(COMMAND ${GIT_VERSION_GEN} .tarball_version
+  execute_process(COMMAND ${GIT_VERSION_GEN} .tarball-version
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     RESULT_VARIABLE _err
     OUTPUT_VARIABLE git_version
@@ -13,24 +16,24 @@ endif()
 if(_err EQUAL 0)
   if(git_version MATCHES
                  "^(0|[1-9][0-9]*)[.](0|[1-9][0-9]*)[.](0|[1-9][0-9]*)[.].*")
-    set(_major "${CMAKE_MATCH_1}")
-    set(_minor "${CMAKE_MATCH_2}")
-    set(_patch "${CMAKE_MATCH_3}")
-    set(PROJECT_VERSION ${_major}.${_minor}.${_patch}.999)
+    set(PROJECT_MAJOR "${CMAKE_MATCH_1}")
+    set(PROJECT_MINOR "${CMAKE_MATCH_2}")
+    set(PROJECT_PATCH "${CMAKE_MATCH_3}")
+    set(PROJECT_VERSION ${PROJECT_MAJOR}.${PROJECT_MINOR}.${PROJECT_PATCH}.999)
   elseif(git_version MATCHES
                  "^(0|[1-9][0-9]*)[.](0|[1-9][0-9]*)[.](0|[1-9][0-9]*)")
-    set(_major "${CMAKE_MATCH_1}")
-    set(_minor "${CMAKE_MATCH_2}")
-    set(_patch "${CMAKE_MATCH_3}")
-    set(PROJECT_VERSION ${_major}.${_minor}.${_patch})
+    set(PROJECT_MAJOR "${CMAKE_MATCH_1}")
+    set(PROJECT_MINOR "${CMAKE_MATCH_2}")
+    set(PROJECT_PATCH "${CMAKE_MATCH_3}")
+    set(PROJECT_VERSION ${PROJECT_MAJOR}.${PROJECT_MINOR}.${PROJECT_PATCH})
   elseif(git_version MATCHES
                  "^(0|[1-9][0-9]*)[.](0|[1-9][0-9]*)")
-    set(_major "${CMAKE_MATCH_1}")
-    set(_minor "${CMAKE_MATCH_2}")
-    set(PROJECT_VERSION ${_major}.${_minor})
+    set(PROJECT_MAJOR "${CMAKE_MATCH_1}")
+    set(PROJECT_MINOR "${CMAKE_MATCH_2}")
+    set(PROJECT_VERSION ${PROJECT_MAJOR}.${PROJECT_MINOR})
   elseif(git_version MATCHES
                  "^(0|[1-9][0-9]*)")
-    set(_major "${CMAKE_MATCH_1}")
-    set(PROJECT_VERSION ${_major})
+    set(PROJECT_MAJOR "${CMAKE_MATCH_1}")
+    set(PROJECT_VERSION ${PROJECT_MAJOR})
   endif()
 endif()
