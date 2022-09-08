87f3ee81923a0185c584dd78848785f4a8ed4539
Tim Griesbach <tim.griesbach@uni-bonn.de>
2022-09-08 13:18:48 +0200

    MPI IO: Fix cleanup for OpenMPI

--- src/p4est_io.c
+++ src/p4est_io.c
@@ -47,7 +47,8 @@
 #define P4EST_FILE_CHECK_OPEN(errcode, fc, user_msg, cperrcode) do {\
                                             SC_CHECK_MPI_VERBOSE (errcode, user_msg);   \
                                             *cperrcode = errcode;                       \
-                                            if (errcode) {SC_FREE (fc->file);           \
+                                            if (errcode) {                              \
+                                            p4est_file_error_cleanup (&fc->file);       \
                                             P4EST_FREE (fc);                            \
                                             p4est_file_error_code (errcode, cperrcode);\
                                             return NULL;}} while (0)
@@ -541,7 +542,7 @@ p4est_file_error_cleanup (sc_MPI_File * file)
   if ((*file)->file != sc_MPI_FILE_NULL) {
 #endif
     /* We do not use here the libsc closing function since we do not perform
-     * error checking in this functiont that is only called if we had already
+     * error checking in this function that is only called if we had already
      * an error.
      */
 #ifdef P4EST_ENABLE_MPIIO
