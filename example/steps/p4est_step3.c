1041010a19173d868836bf2e237cab0b355dc11f
Tim Griesbach <tim.griesbach@uni-bonn.de>
2022-09-26 15:09:36 +0200

    example-restart: Fix long versions of command line paramters

--- example/steps/p4est_step3.c
+++ example/steps/p4est_step3.c
@@ -1489,9 +1489,9 @@ main (int argc, char **argv)
 
   /* read command line options */
   opt = sc_options_new (argv[0]);
-  sc_options_add_bool (opt, 'C', "write checkpoints", &step3_checkpoint, 0,
+  sc_options_add_bool (opt, 'C', "write-checkpoints", &step3_checkpoint, 0,
                        "Write checkpoint files to disk");
-  sc_options_add_string (opt, 'l', "load/start a checkpoint file", &filename,
+  sc_options_add_string (opt, 'l', "load-checkpoint", &filename,
                          NULL, "Load and start from a checkpoint file");
   sc_options_parse (p4est_package_id, SC_LP_DEFAULT, opt, argc, argv);
   sc_options_print_usage (p4est_package_id, SC_LP_ERROR, opt, NULL);
