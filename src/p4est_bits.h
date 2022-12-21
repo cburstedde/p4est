0af83f4a9373d69b7d9157ba19789a6d0540f3db
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2022-12-21 17:03:56 +0100

    Update p4est_bits.h

--- src/p4est_bits.h
+++ src/p4est_bits.h
@@ -766,18 +766,19 @@ void                p4est_neighbor_transform_quadrant_reverse
   (const p4est_neighbor_transform_t * nt,
    const p4est_quadrant_t * neigh_quad, p4est_quadrant_t * self_quad);
 
-/** Check if a descendant shares a face with an ancestor.
+/** Check if a descendant shares a face with a (strict) ancestor.
  *
  * \param [in]  descendant   The descendant in question.
- * \param [in]  ancestor     The ancestor.
+ * \param [in]  ancestor     The ancestor must not be equal to the descendant.
  * \param [in]  face         The face of the descendant.
  *
- * \return  1 if descendant face touches ancestor face else 0.
+ * \return true if descendant face touches ancestor face, false otherwise.
 */
 int                 p4est_quadrant_is_ancestor_face (const p4est_quadrant_t *
                                                      descendant,
                                                      const p4est_quadrant_t *
                                                      ancestor, int face);
+                                                     
 SC_EXTERN_C_END;
 
 #endif /* !P4EST_BITS_H */
