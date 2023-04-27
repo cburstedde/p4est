3c7ac28afd0e5264b7d79e13e169f47314cd449f
Pierre Kestener <pierre.kestener@cea.fr>
2023-04-27 16:53:59 +0200

    Allow to build p4est by detecting libsc using pkgconfig.

--- cmake/find_sc.cmake
+++ cmake/find_sc.cmake
@@ -1,5 +1,5 @@
 # The purpose of this cmake macro is to detect libsc from environnment
-# and make target SC:SC available
+# and make target SC::SC available
 # We are using 3 types of detections:
 # - find_package in CONFIG mode
 # - find_package in MODULE mode
@@ -30,7 +30,26 @@ if (NOT SC_FOUND)
 
     if (P4EST_NEEDS_LIBSC_FOUND)
       message(STATUS "libsc found via pkg-config in PKG_CONFIG_PATH")
-      add_library(SC::SC ALIAS PkgConfig::P4EST_NEEDS_LIBSC)
+
+      # we can't make an alias library here, because it is read only, and thus can't be used
+      # later in top-level CMakeLists.txt to interface with required dependencies
+      #add_library(SC::SC ALIAS PkgConfig::P4EST_NEEDS_LIBSC)
+
+      # so just re-create target SC::SC from scratch, by copying properties from pkgconfig
+      get_target_property(SC_INTERFACE_INCLUDE_DIRECTORIES PkgConfig::P4EST_NEEDS_LIBSC INTERFACE_INCLUDE_DIRECTORIES)
+      get_target_property(SC_INTERFACE_LINK_LIBRARIES PkgConfig::P4EST_NEEDS_LIBSC INTERFACE_LINK_LIBRARIES)
+      add_library(SC::SC INTERFACE IMPORTED)
+      set_property(TARGET SC::SC PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${SC_INTERFACE_INCLUDE_DIRECTORIES}")
+      set_property(TARGET SC::SC PROPERTY INTERFACE_LINK_LIBRARIES "${SC_INTERFACE_LINK_LIBRARIES}")
+
+      # does libsc has mpi support ?
+      execute_process(COMMAND ${PKG_CONFIG_EXECUTABLE} --variable have_mpi libsc
+        OUTPUT_VARIABLE SC_HAVE_MPI)
+
+      # does libsc has json support ?
+      execute_process(COMMAND ${PKG_CONFIG_EXECUTABLE} --variable have_json libsc
+        OUTPUT_VARIABLE SC_HAVE_JSON)
+
       set(SC_FOUND True)
       set(SC_FOUND_VIA_PKGCONFIG True)
     endif()
@@ -40,6 +59,16 @@ if (NOT SC_FOUND)
 
 endif()
 
-if(NOT SC_FOUND)
-  message(STATUS "libsc was not found in environment, we will try to build it.")
+if(SC_HAVE_JSON OR SC_have_json)
+  include(cmake/jansson.cmake)
+  if(NOT jansson_FOUND)
+    message(FATAL_ERROR "libsc requires jansson.")
+  endif()
+endif()
+
+if (mpi AND NOT SC_HAVE_MPI)
+  # notice find_package(MPI) already in top-level CMakeLists.txt, don't need to do it again here.
+
+  # this is actually a fatal error, but could be made a warning (TODO)
+  message(WARNING "You enabled MPI at p4est level, but libsc was not compiled with MPI support.")
 endif()
