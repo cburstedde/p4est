b67d5426e26c610ed38ce692ad7668423e636581
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2023-08-11 14:46:37 +0200

    p4est_connectivity: blockquote one of the docs

--- src/p4est_connectivity.h
+++ src/p4est_connectivity.h
@@ -461,9 +461,12 @@ p4est_connectivity_t *p4est_connectivity_new_moebius (void);
 p4est_connectivity_t *p4est_connectivity_new_star (void);
 
 /** Create a connectivity structure for the six sides of a unit cube.
- * The ordering of the trees is as follows: 0 1
- *                                            2 3 <-- 3: axis-aligned top side
- *                                              4 5.
+ * The ordering of the trees is as follows:
+ *
+ *     0 1
+ *       2 3 <-- 3: axis-aligned top side
+ *         4 5
+ *
  * This choice has been made for maximum symmetry (see tree_to_* in .c file).
  */
 p4est_connectivity_t *p4est_connectivity_new_cubed (void);
