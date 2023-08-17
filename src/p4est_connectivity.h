9094737c8f7f55fde985d91ca9904e3fa1aac050
Hongli Lin <lin@ins.uni-bonn.de>
2023-08-17 11:06:31 +0000

    Use markdown for docs in src/p4est_connectivity.h

--- src/p4est_connectivity.h
+++ src/p4est_connectivity.h
@@ -518,13 +518,13 @@ p4est_connectivity_t *p4est_connectivity_new_disk (int periodic_a,
  * The flat connectivity looks like that:
  * Vextex numbering:
  *
- *    A00   A01   A02   A03   A04
- *   /   \ /   \ /   \ /   \ /   \
- * A05---A06---A07---A08---A09---A10
- *   \   / \   / \   / \   / \   / \
- *    A11---A12---A13---A14---A15---A16
- *      \  /  \  /  \  /  \  /  \  /
- *      A17   A18   A19   A20   A21
+ *        A00   A01   A02   A03   A04
+ *       /   \ /   \ /   \ /   \ /   \
+ *     A05---A06---A07---A08---A09---A10
+ *       \   / \   / \   / \   / \   / \
+ *        A11---A12---A13---A14---A15---A16
+ *          \  /  \  /  \  /  \  /  \  /
+ *          A17   A18   A19   A20   A21
  *
  * Origin in A05.
  *
