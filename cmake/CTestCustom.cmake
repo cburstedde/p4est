d9893ff7f6ad7e0d1664334c7afca78ffce304ee
Mikhail Kirilin <kirilin@ins.uni-bonn.de>
2023-09-24 14:35:23 +0200

    cmake: exclude examples from ctest

---
+++ cmake/CTestCustom.cmake
@@ -0,0 +1,13 @@
+#We add here exmaples we want to exclude from ctest
+set(CTEST_CUSTOM_TESTS_IGNORE
+  balance_seeds2 balance_seeds3
+  mesh2 mesh3
+  periodicity3
+  simple2 simple3
+  bricks2 bricks3
+  timings2 timings3
+  spheres2 spheres3
+  particles2 particles3
+  p4est_step1 p4est_step3 p4est_step4
+  p8est_step1 p8est_step3 p8est_step4
+)
