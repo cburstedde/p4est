1c6a9fb04ef4ed0e542c068d8a655c3bbee5ccc4
Hannes Brandt <brandt@ins.uni-bonn.de>
2023-10-28 21:34:26 +0200

    test hanging-corners: add intertree case

--- test/test_mesh_corners3.c
+++ test/test_mesh_corners3.c
@@ -9,7 +9,8 @@ static
 int refine(p8est_t* p4est, p4est_topidx_t which_tree, p8est_quadrant_t* quadrant)
 {
     int refine = TRUE;
-    if(quadrant->x == 0 && quadrant->y == 0 && quadrant->z == 0)
+    if(quadrant->x == 0 && quadrant->y == 0 && quadrant->z == 0 &&
+       which_tree == 3)
     {
         refine = FALSE;
     }
@@ -41,10 +42,10 @@ int main(int argc, char **argv)
     sc_init (mpicomm, 1, 1, NULL, SC_LP_DEFAULT);
     p4est_init (NULL, SC_LP_DEFAULT);
 
-    p8est_connectivity_t* conn = p8est_connectivity_new_unitcube();
+    p8est_connectivity_t* conn = p8est_connectivity_new_brick(2, 2, 1, 0, 0, 0);
     p8est_t* p8est = p8est_new_ext(mpicomm, conn, 0, 1, 0, 0, NULL, NULL);
 
-    // refine all quadrents to level 2 except quadrent touching 0,0,0
+    // refine all quadrents to level 2 except quadrent in tree 3 touching 0,0,0
     p8est_refine_ext(p8est, FALSE, 2, refine, NULL, NULL);
 
     p8est_ghost_t* ghost = p8est_ghost_new(p8est, P8EST_CONNECT_FULL);
@@ -55,28 +56,36 @@ int main(int argc, char **argv)
     params.btype = P8EST_CONNECT_FULL;
     params.edgehanging_corners = 1;
     p8est_mesh_t* mesh = p8est_mesh_new_params (p8est, ghost, &params);
-    P4EST_ASSERT(mesh->local_num_quadrants == 57);
+    P4EST_ASSERT(mesh->local_num_quadrants == 249);
 
-    // assuming that the coarse quadrent is zero with ordering
-    p4est_locidx_t coarse_qid = 0;
+    // assuming that the coarse quadrent is zero in tree 3 with ordering
+    p4est_locidx_t coarse_qid = 192;
 
-    // upper x,y,z index
+    // upper and lower x,y,z indices
+    p4est_locidx_t lx_idx = P8EST_FACES * coarse_qid + 0;
     p4est_locidx_t ux_idx = P8EST_FACES * coarse_qid + 1;
+    p4est_locidx_t ly_idx = P8EST_FACES * coarse_qid + 2;
     p4est_locidx_t uy_idx = P8EST_FACES * coarse_qid + 3;
     p4est_locidx_t uz_idx = P8EST_FACES * coarse_qid + 5;
 
+    P4EST_ASSERT(mesh->quad_to_face[lx_idx] < 0);
     P4EST_ASSERT(mesh->quad_to_face[ux_idx] < 0);
+    P4EST_ASSERT(mesh->quad_to_face[ly_idx] < 0);
     P4EST_ASSERT(mesh->quad_to_face[uy_idx] < 0);
     P4EST_ASSERT(mesh->quad_to_face[uz_idx] < 0);
 
     // get the quadrents on upper x,y,z faces of coarse quadrent
-    p4est_locidx_t qtq_ux, qtq_uy, qtq_uz;
+    p4est_locidx_t qtq_lx, qtq_ux, qtq_ly, qtq_uy, qtq_uz;
+    qtq_lx = mesh->quad_to_quad[lx_idx];
     qtq_ux = mesh->quad_to_quad[ux_idx];
+    qtq_ly = mesh->quad_to_quad[ly_idx];
     qtq_uy = mesh->quad_to_quad[uy_idx];
     qtq_uz = mesh->quad_to_quad[uz_idx];
 
-    p4est_locidx_t *qth_ux, *qth_uy, *qth_uz;
+    p4est_locidx_t *qth_lx, *qth_ux, *qth_ly, *qth_uy, *qth_uz;
+    qth_lx = (p4est_locidx_t*) sc_array_index(mesh->quad_to_half,qtq_lx);
     qth_ux = (p4est_locidx_t*) sc_array_index(mesh->quad_to_half,qtq_ux);
+    qth_ly = (p4est_locidx_t*) sc_array_index(mesh->quad_to_half,qtq_ly);
     qth_uy = (p4est_locidx_t*) sc_array_index(mesh->quad_to_half,qtq_uy);
     qth_uz = (p4est_locidx_t*) sc_array_index(mesh->quad_to_half,qtq_uz);
 
@@ -84,6 +93,7 @@ int main(int argc, char **argv)
     p4est_locidx_t c1, c2;
     p4est_locidx_t qtc1, qtc2;
 
+    /*** test intra tree corners ***/
     //six pairs of corners with missing information
 
     //two along upper x upper y edge of coarse quad
@@ -142,6 +152,113 @@ int main(int argc, char **argv)
     qid2 = qth_uz[2]; //lower quad along edge on z face
     c1 = 4; // qid1 should have neighbor on uz ly lx corner
     c2 = 3; // qid2 should have neighbor on lz uy ux corner
+    qtc1 = mesh->quad_to_corner[P8EST_CHILDREN*qid1+c1];
+    qtc2 = mesh->quad_to_corner[P8EST_CHILDREN*qid2+c2];
+    num_errors += check_corner(qid1,c1,qtc1,qid2);
+    num_errors += check_corner(qid2,c2,qtc2,qid1);
+
+
+    /*** test inter-tree corners across tree-face ***/
+    //eight pairs of corners with missing information
+
+    //two along lower x upper y edge of coarse quad (across face to tree 2)
+
+    qid1 = qth_lx[1]; //lower quad along edge on x face of coarse quad
+    qid2 = qth_uy[2]; //upper quad along edge on y face of coarse quad
+    c1 = 7; // qid1 should have neighbor on uz uy ux corner
+    c2 = 0; // qid2 should have neighbor on lz ly lx corner
+    qtc1 = mesh->quad_to_corner[P8EST_CHILDREN*qid1+c1];
+    qtc2 = mesh->quad_to_corner[P8EST_CHILDREN*qid2+c2];
+    num_errors += check_corner(qid1,c1,qtc1,qid2);
+    num_errors += check_corner(qid2,c2,qtc2,qid1);
+
+    qid1 = qth_lx[3]; //upper quad along edge on x face of coarse quad
+    qid2 = qth_uy[0]; //lower quad along edge on y face of coarse quad
+    c1 = 3; // qid1 should have neighbor on lz uy ux corner
+    c2 = 4; // qid2 should have neighbor on uz ly lx corner
+    qtc1 = mesh->quad_to_corner[P8EST_CHILDREN*qid1+c1];
+    qtc2 = mesh->quad_to_corner[P8EST_CHILDREN*qid2+c2];
+    num_errors += check_corner(qid1,c1,qtc1,qid2);
+    num_errors += check_corner(qid2,c2,qtc2,qid1);
+
+    //two along lower x upper z edge of coarse quad (across face to tree 2)
+
+    qid1 = qth_lx[2]; //lower quad along edge on x face of coarse quad
+    qid2 = qth_uz[2]; //upper quad along edge on z face of coarse quad
+    c1 = 7; // qid1 should have neighbor on uz uy ux corner
+    c2 = 0; // qid2 should have neighbor on lz ly lx corner
+    qtc1 = mesh->quad_to_corner[P8EST_CHILDREN*qid1+c1];
+    qtc2 = mesh->quad_to_corner[P8EST_CHILDREN*qid2+c2];
+    num_errors += check_corner(qid1,c1,qtc1,qid2);
+    num_errors += check_corner(qid2,c2,qtc2,qid1);
+
+    qid1 = qth_lx[3]; //upper quad along edge on x face of coarse quad
+    qid2 = qth_uz[0]; //lower quad along edge on z face of coarse quad
+    c1 = 5; // qid1 should have neighbor on uz ly ux corner
+    c2 = 2; // qid2 should have neighbor on lz uy lx corner
+    qtc1 = mesh->quad_to_corner[P8EST_CHILDREN*qid1+c1];
+    qtc2 = mesh->quad_to_corner[P8EST_CHILDREN*qid2+c2];
+    num_errors += check_corner(qid1,c1,qtc1,qid2);
+    num_errors += check_corner(qid2,c2,qtc2,qid1);
+
+    //two along upper x lower y edge of coarse quad (across face to tree 1)
+
+    qid1 = qth_ux[0]; //lower quad along edge on x face of coarse quad
+    qid2 = qth_ly[3]; //upper quad along edge on y face of coarse quad
+    c1 = 4; // qid1 should have neighbor on uz ly lx corner
+    c2 = 3; // qid2 should have neighbor on lz uy ux corner
+    qtc1 = mesh->quad_to_corner[P8EST_CHILDREN*qid1+c1];
+    qtc2 = mesh->quad_to_corner[P8EST_CHILDREN*qid2+c2];
+    num_errors += check_corner(qid1,c1,qtc1,qid2);
+    num_errors += check_corner(qid2,c2,qtc2,qid1);
+
+    qid1 = qth_ux[2]; //upper quad along edge on x face of coarse quad
+    qid2 = qth_ly[1]; //lower quad along edge on y face of coarse quad
+    c1 = 0; // qid1 should have neighbor on lz ly lx corner
+    c2 = 7; // qid2 should have neighbor on uz uy ux corner
+    qtc1 = mesh->quad_to_corner[P8EST_CHILDREN*qid1+c1];
+    qtc2 = mesh->quad_to_corner[P8EST_CHILDREN*qid2+c2];
+    num_errors += check_corner(qid1,c1,qtc1,qid2);
+    num_errors += check_corner(qid2,c2,qtc2,qid1);
+
+    //two along lower y upper z edge of coarse quad (across face to tree 1)
+
+    qid1 = qth_ly[2]; //lower quad along edge on y face of coarse quad
+    qid2 = qth_uz[1]; //upper quad along edge on z face of coarse quad
+    c1 = 7; // qid1 should have neighbor on uz uy ux corner
+    c2 = 0; // qid2 should have neighbor on lz ly lx corner
+    qtc1 = mesh->quad_to_corner[P8EST_CHILDREN*qid1+c1];
+    qtc2 = mesh->quad_to_corner[P8EST_CHILDREN*qid2+c2];
+    num_errors += check_corner(qid1,c1,qtc1,qid2);
+    num_errors += check_corner(qid2,c2,qtc2,qid1);
+
+    qid1 = qth_ly[3]; //upper quad along edge on y face of coarse quad
+    qid2 = qth_uz[0]; //lower quad along edge on z face of coarse quad
+    c1 = 6; // qid1 should have neighbor on uz uy lx corner
+    c2 = 1; // qid2 should have neighbor on lz ly ux corner
+    qtc1 = mesh->quad_to_corner[P8EST_CHILDREN*qid1+c1];
+    qtc2 = mesh->quad_to_corner[P8EST_CHILDREN*qid2+c2];
+    num_errors += check_corner(qid1,c1,qtc1,qid2);
+    num_errors += check_corner(qid2,c2,qtc2,qid1);
+
+    /*** test inter-tree corners across tree-edge ***/
+    //two pairs of corners with missing information
+
+    //two along lower x lower y edge of coarse quad (across edge to tree 0)
+
+    qid1 = qth_lx[0]; //lower quad along edge on x face of coarse quad
+    qid2 = qth_ly[2]; //upper quad along edge on y face of coarse quad
+    c1 = 5; // qid1 should have neighbor on uz ly ux corner
+    c2 = 2; // qid2 should have neighbor on lz uy lx corner
+    qtc1 = mesh->quad_to_corner[P8EST_CHILDREN*qid1+c1];
+    qtc2 = mesh->quad_to_corner[P8EST_CHILDREN*qid2+c2];
+    num_errors += check_corner(qid1,c1,qtc1,qid2);
+    num_errors += check_corner(qid2,c2,qtc2,qid1);
+
+    qid1 = qth_lx[2]; //upper quad along edge on x face of coarse quad
+    qid2 = qth_ly[0]; //lower quad along edge on y face of coarse quad
+    c1 = 1; // qid1 should have neighbor on lz ly ux corner
+    c2 = 6; // qid2 should have neighbor on uz uy lx corner
     qtc1 = mesh->quad_to_corner[P8EST_CHILDREN*qid1+c1];
     qtc2 = mesh->quad_to_corner[P8EST_CHILDREN*qid2+c2];
     num_errors += check_corner(qid1,c1,qtc1,qid2);
