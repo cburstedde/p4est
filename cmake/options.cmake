ec38c3f479fe4c3b1f64e40fe386aac1e63910c4
scivision <scivision@users.noreply.github.com>
2023-11-01 16:50:54 -0400

    switch to new packaging

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,7 +2,6 @@ option(enable_p6est "build p6est" on)
 option(enable_p8est "build p8est" on)
 
 option(enable-file-deprecated "use depreacted data file format" off)
-option(sc_external "build sc library" ON)
 
 option(vtk_binary "VTK binary interface" on)
 if(vtk_binary)
