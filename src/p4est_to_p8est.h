a6eb2a51b436889a198216cbc13073cb9b6be60c
ljcarlin <louisjcarlin98@gmail.com>
2024-03-16 11:50:12 +0100

    Feature search_transfer: gfx and gfp

--- src/p4est_to_p8est.h
+++ src/p4est_to_p8est.h
@@ -490,6 +490,8 @@
 #define p4est_transfer_items_end        p8est_transfer_items_end
 #define p4est_transfer_end              p8est_transfer_end
 #define p4est_transfer_search           p8est_transfer_search
+#define p4est_transfer_search_gfx       p8est_transfer_search_gfx
+#define p4est_transfer_search_gfp       p8est_transfer_search_gfp
 #define p4est_transfer_search_destroy   p8est_transfer_search_destroy
 
 /* functions in p4est_io */
