eb90738f2b9933a2e6bd65a55272c6589b7aeb26
Tim Griesbach <tim.griesbach@uni-bonn.de>
2025-02-07 13:16:18 +0100

    test_io: Call init functions for any configuration

--- test/test_io2.c
+++ test/test_io2.c
@@ -209,10 +209,10 @@ compressed_quadrant_t;
 int
 main (int argc, char **argv)
 {
-#ifdef P4EST_ENABLE_FILE_DEPRECATED
-
   sc_MPI_Comm         mpicomm;
-  int                 mpiret, errcode;
+  int                 mpiret;
+#ifdef P4EST_ENABLE_FILE_DEPRECATED
+  int                 errcode;
   int                 rank, size;
   int                 level = 3;
   int                 empty_header, read_only, header_only;
@@ -239,18 +239,23 @@ main (int argc, char **argv)
   sc_options_t       *opt;
   char                current_user_string[P4EST_FILE_USER_STRING_BYTES];
   compressed_quadrant_t *qr, *qs;
+#endif /* P4EST_ENABLE_FILE_DEPRECATED */
 
   /* initialize MPI */
   mpiret = sc_MPI_Init (&argc, &argv);
   SC_CHECK_MPI (mpiret);
   mpicomm = sc_MPI_COMM_WORLD;
+
+  sc_init (mpicomm, 1, 1, NULL, SC_LP_DEFAULT);
+  p4est_init (NULL, SC_LP_DEFAULT);
+
+#ifdef P4EST_ENABLE_FILE_DEPRECATED
+
   mpiret = sc_MPI_Comm_size (mpicomm, &size);
   SC_CHECK_MPI (mpiret);
   mpiret = sc_MPI_Comm_rank (mpicomm, &rank);
   SC_CHECK_MPI (mpiret);
 
-  sc_init (mpicomm, 1, 1, NULL, SC_LP_DEFAULT);
-  p4est_init (NULL, SC_LP_DEFAULT);
 
   /* parse command line parameters */
   opt = sc_options_new (argv[0]);
@@ -635,12 +640,12 @@ main (int argc, char **argv)
   sc_array_reset (&elem_size);
   sc_options_destroy (opt);
 
+#endif /* P4EST_ENABLE_FILE_DEPRECATED */
+
   sc_finalize ();
 
   mpiret = sc_MPI_Finalize ();
   SC_CHECK_MPI (mpiret);
-
-#endif /* P4EST_ENABLE_FILE_DEPRECATED */
 
   return 0;
 }
