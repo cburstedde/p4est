7dd6355f3607407abba9b2b76af4b7cf5059c501
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2026-06-01 17:06:40 +0200

    Feature triangle: write both Q1 and Q2 meshes

--- example/mesh/tnodes2.c
+++ example/mesh/tnodes2.c
@@ -176,13 +176,11 @@ refine_normal (p4est_t *p4est, p4est_topidx_t which_tree,
 static void
 tnodes_run_Q1 (p4est_t *p4est, p4est_geometry_t *geom, p4est_ghost_t *ghost)
 {
+  int                 retval;
   double              Q1time;
   p4est_lnodes_t     *ln;
   p4est_tnodes_t     *tm;
-#if 0
-  int                 retval;
   p4est_vtk_context_t *cont;
-#endif
 
   P4EST_GLOBAL_PRODUCTION ("Example tnodes run Q1\n");
 
@@ -200,18 +198,17 @@ tnodes_run_Q1 (p4est_t *p4est, p4est_geometry_t *geom, p4est_ghost_t *ghost)
   Q1time = sc_MPI_Wtime () - Q1time;
   sc_stats_set1 (stats + MESH_TNODES_STATS_Q1, Q1time, "Q1");
 
-#if 0
   if (!novtk) {
     /* write VTK output */
 
-    /* the geometry was passed to the tnodes already, don't use it here */
-    cont = p4est_vtk_context_new (p4est, P4EST_STRING "_tnodes_simplices");
+    cont = p4est_vtk_context_new (p4est, P4EST_STRING "_tnodes_Q1_simplices");
     SC_CHECK_ABORT (cont != NULL, "Open VTK context");
+    p4est_vtk_context_set_lnodes (cont, ln);
     p4est_vtk_context_set_geom (cont, geom);
     p4est_vtk_context_set_continuous (cont, 1);
 
     /* beware: values < 1. cause a lot more mesh nodes */
-    p4est_vtk_context_set_scale (cont, .9);
+    p4est_vtk_context_set_scale (cont, 1.);
 
     cont = p4est_vtk_write_header_tnodes (cont, tm);
     SC_CHECK_ABORT (cont != NULL, "Write tnodes VTK header");
@@ -220,7 +217,6 @@ tnodes_run_Q1 (p4est_t *p4est, p4est_geometry_t *geom, p4est_ghost_t *ghost)
     retval = p4est_vtk_write_footer (cont);
     SC_CHECK_ABORT (!retval, "Close VTK context");
   }
-#endif
 
   /* free triangle mesh */
   p4est_tnodes_destroy (tm);
@@ -270,8 +266,8 @@ compare_both_Q2_constructions (p4est_t *p4est, p4est_lnodes_t *ln,
 static void
 tnodes_run_Q2 (p4est_t *p4est, p4est_geometry_t *geom, p4est_ghost_t *ghost)
 {
-  double              Q2Itime, Q2Etime;
   int                 retval;
+  double              Q2Itime, Q2Etime;
   size_t              sm;
   p4est_lnodes_t     *ln;
   p4est_tnodes_t     *tm;
@@ -306,8 +302,7 @@ tnodes_run_Q2 (p4est_t *p4est, p4est_geometry_t *geom, p4est_ghost_t *ghost)
   if (!novtk) {
     /* write VTK output */
 
-    /* the geometry was passed to the tnodes already, don't use it here */
-    cont = p4est_vtk_context_new (p4est, P4EST_STRING "_tnodes_simplices");
+    cont = p4est_vtk_context_new (p4est, P4EST_STRING "_tnodes_Q2_simplices");
     SC_CHECK_ABORT (cont != NULL, "Open VTK context");
     p4est_vtk_context_set_lnodes (cont, ln);
     p4est_vtk_context_set_geom (cont, geom);
